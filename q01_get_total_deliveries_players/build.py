fdfdef839b520a49839858b1dbc9e3f90367fcb8
Jay Trivedi <mail.jaytrivedi@gmail.com>
2017-10-02 00:28:51 +0530

    Update build.py

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -1,14 +1,7 @@
 # Default imports
-import sys, os
-sys.path.append(os.path.join(os.path.dirname(os.curdir), '..'))
-
 import numpy as np
 
-def read_ipl_data(path, dtype=np.float64):
-    return np.genfromtxt(path, dtype=dtype, skip_header=1, delimiter=",")
-
-ipl_matches_array = read_ipl_data("./data/ipl_matches_small.csv", '|S50')
-
+ipl_matches_array =np.genfromtxt("./data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
 
 # Your Solution
