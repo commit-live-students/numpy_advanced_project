6085a206584d1d8d24fa043d6d3a01307087a05e
rameshgayam <ramesh.gayam@gmail.com>
2017-10-05 07:54:32 +0000

    Done

--- q04_get_all_sixes_filter/build.py
+++ q04_get_all_sixes_filter/build.py
@@ -1,7 +1,6 @@
-#Default Imports
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 import numpy as np
-
+ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 #Your Solution
 def get_all_sixes_filter():
     return (ipl_matches_array[:, 16].astype(np.int16) == 6)
