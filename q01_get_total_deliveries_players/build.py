605ff403db8c597a7a9a66c21be045188970b61a
sudharsanasai <Sudharsanasai@gmail.com>
2017-10-05 08:19:03 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -4,7 +4,14 @@ import numpy as np
 ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
 # Your Solution
-def get_total_deliveries_played(batsman):
-    ipl_matches_array_batsman = ipl_matches_array[:,13]
 
-    return sum(ipl_matches_array_batsman == batsman)
+def get_wicket_delivery_numbers_array(player):
+    #print(player)
+    batsman = ipl_matches_array[ipl_matches_array[:,13]==player]
+    #print(batsman)
+    #outballs = np.where(batsman[:-3]==player)
+    #count = 0
+    balls = batsman[:,11][batsman[:,-3]==player]
+
+    #if batsman[:-3]=='ST Jayasuriya':
+    return balls
--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -3,6 +3,16 @@ import numpy as np
 
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 
-def get_wicket_delivery_numbers_array(batsman):
-    return ipl_matches_array[:,11][(ipl_matches_array[:,13] == batsman) & (ipl_matches_array[:,-3]!= batsman)]
+def get_wicket_delivery_numbers_array(player):
+    #print(player)
+    batsman = ipl_matches_array[ipl_matches_array[:,13]==player]
+    #print(batsman)
+    #outballs = np.where(batsman[:-3]==player)
+    #count = 0
+    balls = batsman[:,11][batsman[:,-3]==player]
+
+    #if batsman[:-3]=='ST Jayasuriya':
+    return balls
+#print(get_wicket_delivery_numbers_array('SC Ganguly'))
+
 #Your Solution
