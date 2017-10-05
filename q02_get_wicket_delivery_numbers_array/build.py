3d6b10c8bb6a5e2aa990f74e1a23f270f81c22ba
sudharsanasai <Sudharsanasai@gmail.com>
2017-10-05 03:47:55 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -3,4 +3,6 @@ import numpy as np
 
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 
+def get_wicket_delivery_numbers_array(batsman):
+    return ipl_matches_array[:,11][(ipl_matches_array[:,13] == batsman) & (ipl_matches_array[:,-3]!= batsman)]
 #Your Solution
--- q03_get_toss_win_count/build.py
+++ q03_get_toss_win_count/build.py
@@ -2,3 +2,12 @@
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 
 #Your Solution
+import numpy as np
+#Your Solution
+
+def get_toss_win_count(team='Mumbai Indians'):
+    ipl_matches_array_toss_mum = ipl_matches_array[ipl_matches_array[:,5]=='Mumbai Indians']
+    combs = ipl_matches_array_toss_mum[:,[0,5]]
+
+    b = np.vstack({tuple(row) for row in combs})
+    return b.shape[0]
