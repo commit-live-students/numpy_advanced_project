5c2506b74f532ac0e8b76ae11c7b1b0a37340d67
satishkhanna <snykhanna@gmail.com>
2017-10-07 18:16:03 +0000

    Done

--- q04_get_all_sixes_filter/build.py
+++ q04_get_all_sixes_filter/build.py
@@ -3,3 +3,8 @@ from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ip
 import numpy as np
 
 #Your Solution
+ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+def get_all_sixes_filter():
+    filter1 = ipl_matches_array[:,16].astype(np.int16) == 6
+    sixer_match = ipl_matches_array[filter1]
+    return sixer_match
--- q05_create_delivery_series/build.py
+++ q05_create_delivery_series/build.py
@@ -1,5 +1,11 @@
 #Default Imports
 import pandas as pd
+import numpy as np
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 
 #Your Solution
+ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+def create_delivery_series():
+    a = ipl_matches_array[:,11]
+    del_ser = pd.Series(a)
+    return del_ser
