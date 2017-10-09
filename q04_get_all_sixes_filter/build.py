04ea8cfe4c5a8be4944007ceede54f7949a23e69
samir0719 <sameer_0719@yahoo.com>
2017-10-09 07:46:29 +0000

    Done

--- q04_get_all_sixes_filter/build.py
+++ q04_get_all_sixes_filter/build.py
@@ -8,4 +8,5 @@ def get_all_sixes_filter():
     ipl_matches_array = np.genfromtxt(path,dtype='|S50',delimiter=',',skip_header=1)
 
     fliter_6 = ipl_matches_array[0:,16] == '6'
-    return np.unique(ipl_matches_array[fliter_6][0:,0])
+    #return np.unique(ipl_matches_array[fliter_6][0:,0])
+    return fliter_6
