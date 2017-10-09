740150e2280f425aac6c2f627e2e67daa76b76bd
arushikumar <arushikumar14@gmail.com>
2017-10-09 18:03:32 +0000

    Done

--- q03_get_toss_win_count/build.py
+++ q03_get_toss_win_count/build.py
@@ -1,7 +1,10 @@
 #Default Imports
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 import numpy as np
+import pandas as pd
 #Your Solution
 def get_toss_win_count(team = "Mumbai Indians"):
     flt_team = ipl_matches_array[:,5] == team
-    return len(ipl_matches_array[flt_team])
+    count = len(np.unique(ipl_matches_array[flt_team][:,0]))
+    #print count
+    return count
--- q05_create_delivery_series/build.py
+++ q05_create_delivery_series/build.py
@@ -3,3 +3,8 @@ import pandas as pd
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 
 #Your Solution
+def create_delivery_series():
+    batsman_ind = ipl_matches_array[:,13]
+    delivery_data = ipl_matches_array[:,11]
+    delivery_series = pd.Series(delivery_data, batsman_ind)
+    return delivery_series
