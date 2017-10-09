a1efa9a6b471464fbe8da4697b5ed7371e173fad
gaurav-barclays <gaurav.simply@gmail.com>
2017-10-09 06:20:17 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,6 +1,13 @@
+# %load q02_get_wicket_delivery_numbers_array/build.py
 #Default Imports
 import numpy as np
 
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 
 #Your Solution
+def get_wicket_delivery_numbers_array(batsman):
+    #data =ipl_matches_array[:,11]
+    all_delivery=ipl_matches_array[:,11]
+    batting_batsman = ipl_matches_array[:,13].astype(np.str)==batsman
+    batsman_out=ipl_matches_array[:,-3].astype(np.str)==batsman
+    return all_delivery[ batsman_out]
