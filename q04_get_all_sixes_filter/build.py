b37eb44cc767b66e41b6deda772b72f4d7689075
swapnilrt <swapnil13488@gmail.com>
2017-10-10 18:14:19 +0000

    Done

--- q04_get_all_sixes_filter/build.py
+++ q04_get_all_sixes_filter/build.py
@@ -1,5 +1,8 @@
+# %load q04_get_all_sixes_filter/build.py
 #Default Imports
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 import numpy as np
 
-#Your Solution
+def get_all_sixes_filter():
+    sixFilter=ipl_matches_array[:,16].astype(np.int16) == 6
+    return sixFilter
