e964c9b543e6b7a614ad34e8d96af73d55bb706f
swapnilrt <swapnil13488@gmail.com>
2017-10-10 17:41:13 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,6 +1,12 @@
+# %load q02_get_wicket_delivery_numbers_array/build.py
 #Default Imports
 import numpy as np
 
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 
-#Your Solution
+def get_wicket_delivery_numbers_array(player):
+    batsmanFilter=ipl_matches_array[:,13]==player
+    outFilter=ipl_matches_array[:,20]==player
+    filterArray=ipl_matches_array[batsmanFilter & outFilter]
+    finalOutput=filterArray[:,11]
+    return finalOutput
