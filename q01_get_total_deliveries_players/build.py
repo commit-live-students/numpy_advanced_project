e70b20a2520f999d0ecdd6c99e82ef536d929ca5
swapnilrt <swapnil13488@gmail.com>
2017-10-10 17:27:10 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -1,7 +1,11 @@
+# %load q01_get_total_deliveries_players/build.py
 # Default imports
 import numpy as np
 
 ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
-# Your Solution
-
+def get_total_deliveries_played(batsman):
+    batsmanFilter=ipl_matches_array[:,13]==batsman
+    filterArray=ipl_matches_array[batsmanFilter]
+    total_runs=np.count_nonzero(filterArray[:,12])
+    return total_runs
