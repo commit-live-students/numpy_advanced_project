acd02b239ebb21e3f5bece2e72eca2f6cd82083f
saylijawale123 <saylijawale123@gmail.com>
2017-10-10 16:45:19 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -3,5 +3,8 @@ import numpy as np
 
 ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
-# Your Solution
-
+def get_total_deliveries_played(batsman):
+    ipl_match = ipl_matches_array[:,13]
+    ipl_match_list = list(ipl_match)
+    count_deliveries= ipl_match_list.count("SR Tendulkar")
+    return count_deliveries
