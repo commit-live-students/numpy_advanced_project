3188344252c99892f74b09aded07682995bc2a61
mritunjaysingh07 <mj.singh07@gmail.com>
2017-10-11 04:00:04 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,6 +1,16 @@
 #Default Imports
+import pandas as pd
 import numpy as np
-
-from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
-
-#Your Solution
+#from numpy import genfromtxt
+def get_wicket_delivery_numbers_array(player) :
+    ipl_matches_array = pd.read_csv("./data/ipl_matches_small.csv", delimiter=',')
+    #print ipl_matches_array.dtype.names
+    #df[(df.a != -1) & (df.b != -1)]
+    #boolarr =  [(ipl_matches_array['batsman'] == batsman) & (ipl_matches_array['player_out'] == batsman)]
+    #ipl_matches_array[c == 'ST Jayasuriya',delievery]
+    # count =  np.sum(boolarr)
+    a= ipl_matches_array.loc[ipl_matches_array['player_out'] == player, 'delivery']
+    a = np.array(a, dtype=pd.Series)
+    #a = ipl_matches_array[boolarr]
+    #print boolarr
+    return a
