ec7b3622e12e7e221062584557e5ea0423134995
mritunjaysingh07 <mj.singh07@gmail.com>
2017-10-11 05:00:54 +0000

    Done

--- q03_get_toss_win_count/build.py
+++ q03_get_toss_win_count/build.py
@@ -2,3 +2,23 @@
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 
 #Your Solution
+import pandas as pd
+import numpy as np
+#from numpy import genfromtxt
+def get_toss_win_count(team ="Mumbai Indians" ) :
+    ipl_matches_array = pd.read_csv("./data/ipl_matches_small.csv", delimiter=',')
+    #print ipl_matches_array.shape
+    #print ipl_matches_array.dtype.names
+    #df[(df.a != -1) & (df.b != -1)]
+    #boolarr =  [(ipl_matches_array['batsman'] == batsman) & (ipl_matches_array['player_out'] == batsman)]
+    #ipl_matches_array[c == 'ST Jayasuriya',delievery]
+    # count =  np.sum(boolarr)
+   # grp = ipl_matches_array.groupby(['match_code'])
+    a = ipl_matches_array[ipl_matches_array['toss_winner'] == team]#.groupby(ipl_matches_array['match_code']).count()
+    count =  a.groupby(ipl_matches_array['match_code']).agg('count')['match_code']
+    count = count.count()
+
+    #a = np.array(a, dtype=pd.Series)
+    #a = ipl_matches_array[boolarr]
+    #print boolarr
+    return count
--- q06_create_runs_series/build.py
+++ q06_create_runs_series/build.py
@@ -1,6 +1,32 @@
 #Default Imports
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
+#%load q06_create_runs_series/build.py
+
+#%load q05_create_delivery_series/build.py
+
 import pandas as pd
+import numpy as np
+#from numpy import genfromtxt
+def create_runs_series(match_code) :
+    ipl_matches_array = pd.read_csv("./data/ipl_matches_small.csv", delimiter=',')
+    df = ipl_matches_array.set_index('delivery') # Set index
+    df = df[df['match_code'] == match_code]
+    #series = (df[['match_code','runs']])
+    series = df['runs']
+    #a= ipl_matches_array.loc[ipl_matches_array['player_out'] == player, 'delivery']
+    #df = df.loc[df[:,'match_code','runs']]
+    #print ipl_matches_array.dtype.names
+    #df[(df.a != -1) & (df.b != -1)]
+    #boolarr =  [(ipl_matches_array['runs'] == 6)]
+    #boolarr = np.array(boolarr)
+    #series = ipl_matches_array.loc[ipl_matches_array[:,delievery]]
+    # count =  np.sum(boolarr)
+    #a= ipl_matches_array['delivery']
+    #a = np.array(a, dtype=pd.Series)
+    #a = ipl_matches_array[boolarr]
+    return series
+#create_runs_series(392203)
+
 
 
 #Your Solution
