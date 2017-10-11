5297aaedc9e1e117a8c9238cfcafc62ad0545c51
shahprachi94 <prachirshah1994@gmail.com>
2017-10-11 15:47:22 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,6 +1,23 @@
+# %load q02_get_wicket_delivery_numbers_array/build.py
 #Default Imports
 import numpy as np
 
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
+#ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+def get_wicket_delivery_numbers_array(player):
+
+    playerFilter = (ipl_matches_array[:,-3] == player)
+
+    final = ipl_matches_array[:,-12]
+
+    finaldeliveries = final[playerFilter]
+    return finaldeliveries
+
+
+#     unique, counts = np.unique(final, return_counts=True)
+#     print unique
+#     print(counts)
+#get_wicket_delivery_numbers_array('ST Jayasuriya')
+    
 
 #Your Solution
