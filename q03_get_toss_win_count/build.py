1943490c906dd0f83b4d26dbc80d1d025371bcac
ashwinij9 <ashwinijoshi9793@gmail.com>
2017-10-11 16:21:41 +0000

    Done

--- q03_get_toss_win_count/build.py
+++ q03_get_toss_win_count/build.py
@@ -1,4 +1,14 @@
 #Default Imports
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 
-#Your Solution
+def get_toss_win_count(team="Mumbai Indians"):
+
+    count = 0
+    for i in ipl_matches_array:
+        if i[5] == team :
+            count = count + 1
+
+    print(count)
+
+
+get_toss_win_count()
--- q04_get_all_sixes_filter/build.py
+++ q04_get_all_sixes_filter/build.py
@@ -2,4 +2,11 @@
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 import numpy as np
 
-#Your Solution
+def get_all_sixes_filter():
+
+
+    sixes_filter = ipl_matches_array[:,16].astype(np.int)==6
+    return sixes_filter
+
+
+get_all_sixes_filter()
