3dd6ad98670162bfb5d5b776b455648d36f5471c
Sakshi-BUK <sakshi.dhokrat96@gmail.com>
2017-10-15 10:53:07 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,6 +1,25 @@
+# %load q02_get_wicket_delivery_numbers_array/build.py
 #Default Imports
 import numpy as np
 
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
-
+player="ST Jayasuriya"
+t1=[]
 #Your Solution
+def get_wicket_delivery_numbers_array(player):
+    test=ipl_matches_array[1:,[11,20]]
+    out=test[test[:,1]==player]
+    ans=out[:,0]
+    #print(test[:,1])
+#     for i in test:
+#         if (i[1]==player):
+#             t1.append(i[0])
+#         else:
+#             continue
+#         test=np.array(t1)
+#         print(type(test))
+    return(ans)
+
+
+
+get_wicket_delivery_numbers_array(player)
