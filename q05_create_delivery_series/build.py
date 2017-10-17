90ce5e2ff10e5e7a8e5e5e7c3541ebc3179e0926
Rahul0211 <rahuljain86@hotmail.com>
2017-10-17 13:50:36 +0000

    Done

--- q05_create_delivery_series/build.py
+++ q05_create_delivery_series/build.py
@@ -2,4 +2,7 @@
 import pandas as pd
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 
-#Your Solution
+def create_delivery_series():
+    data = pd.Series(ipl_matches_array[:,11])
+    return data
+create_delivery_series()
