3b9ae67537898544b8d95e64fc2fc6ad674ce5a7
darshind <doshi.darshin@gmail.com>
2017-10-22 16:54:26 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,6 +1,10 @@
 #Default Imports
 import numpy as np
 
-from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
-
+#from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
+ipl_matches_array =np.genfromtxt("/home/darshind/Workspace/code/numpy_advanced_project/data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 #Your Solution
+def get_wicket_delivery_numbers_array(batsman):
+    data = ipl_matches_array[ipl_matches_array[:,20] == batsman]
+    return data[:,11]
+print get_wicket_delivery_numbers_array(batsman = 'AC Gilchrist')
