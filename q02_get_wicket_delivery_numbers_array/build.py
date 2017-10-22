aa9406e1eb97ea7c88f62b6d58be250431d203bb
sagard21 <sagard21@gmail.com>
2017-10-22 21:42:36 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -4,3 +4,11 @@ import numpy as np
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 
 #Your Solution
+delivery = ipl_matches_array[:,11]
+bats = ipl_matches_array[:,13]
+out = ipl_matches_array[:,20]
+def get_wicket_delivery_numbers_array(player):
+    arr = []
+    for i in delivery[out == player]:
+        arr.append(i)
+    return arr
