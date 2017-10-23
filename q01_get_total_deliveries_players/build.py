54c45595358c0bc01836441a13ce1a1b4873f554
melviii100 <melvinjose1992@gmail.com>
2017-10-23 08:58:01 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -4,4 +4,6 @@ import numpy as np
 ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
 # Your Solution
-
+def get_total_deliveries_played(batsman):
+    l = [i[0] for i in ipl_matches_array  if i[13] == batsman]
+    return len(l)
