b711c5566a1f656391ad170d533301a1ae6339be
jmcode79 <mukherjee.jeet@gmail.com>
2017-10-24 19:27:08 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -1,7 +1,13 @@
+# %load q01_get_total_deliveries_players/build.py
 # Default imports
 import numpy as np
 
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+ipl_matches_array = np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
-# Your Solution
-
+list_1 = ipl_matches_array[:,13]
+def get_total_deliveries_played(batsman):
+    count = 0
+    for i in list_1:
+        if i == batsman:
+            count = count + 1
+    return count
