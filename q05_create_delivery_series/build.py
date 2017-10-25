e2a8f1dde59cc4cbebca58958d17b9d91c459e98
darshind <doshi.darshin@gmail.com>
2017-10-25 16:10:05 +0000

    Done

--- q05_create_delivery_series/build.py
+++ q05_create_delivery_series/build.py
@@ -1,5 +1,10 @@
 #Default Imports
 import pandas as pd
-from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
+import numpy as np
+#from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
+ipl_matches_array =np.genfromtxt("/home/darshind/Workspace/code/numpy_advanced_project/data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
 #Your Solution
+def create_delivery_series():
+     return pd.Series(ipl_matches_array[:,11])
+print create_delivery_series()
--- q06_create_runs_series/build.py
+++ q06_create_runs_series/build.py
@@ -1,6 +1,11 @@
 #Default Imports
-from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
+#from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 import pandas as pd
+import numpy as np
+ipl_matches_array =np.genfromtxt("/home/darshind/Workspace/code/numpy_advanced_project/data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
 
 #Your Solution
+def create_delivery_series():
+     return pd.Series(ipl_matches_array[:,11])
+print create_delivery_series()
