cb21c22a7dd9e7ce85dd4576dfaed33af5f892c4
AnujaySaraf <anujaysaraf7@gmail.com>
2017-10-25 11:45:28 +0000

    Done

--- q06_create_runs_series/build.py
+++ q06_create_runs_series/build.py
@@ -4,3 +4,7 @@ import pandas as pd
 
 
 #Your Solution
+def create_runs_series(match_code):
+    arr = ipl_matches_array[ ipl_matches_array[:,0] == match_code, 16 ]
+    index = ipl_matches_array[ ipl_matches_array[:,0] == match_code, 11 ]
+    return pd.Series(arr, index=index)
