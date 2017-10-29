e4b764f6c86dd49945423c26376d849bc35431e1
nikhil-ds <prasadnikhil25@gmail.com>
2017-10-29 16:04:15 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,6 +1,20 @@
 #Default Imports
 import numpy as np
+import pandas as pd
 
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 
 #Your Solution
+def get_wicket_delivery_numbers_array(player):
+    df = pd.DataFrame(ipl_matches_array)
+    #print(df.head())
+    df = df[[11,20]]
+    find_player_filter = (df[20] == player)
+    print(find_player_filter)
+    print(df[find_player_filter])
+    out_array = df[find_player_filter][11].values
+    print(type(out_array))
+    return out_array
+
+    #print(df.head(30))
+#get_wicket_delivery_numbers_array('ST Jayasuriya')
--- q03_get_toss_win_count/build.py
+++ q03_get_toss_win_count/build.py
@@ -1,4 +1,14 @@
 #Default Imports
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
-
+import pandas as pd
 #Your Solution
+def get_toss_win_count(team = 'Mumbai Indians'):
+    df = pd.DataFrame(ipl_matches_array)
+    df = df[[0,5]]
+    df.drop_duplicates(keep = 'first', inplace = True)
+    df = df[5]
+    count = df[(df == team)].count()
+    #print(df.head())
+    #print(count)
+    return count
+get_toss_win_count()
