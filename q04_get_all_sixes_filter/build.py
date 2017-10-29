a084864cc925b5e82c5c8a519555d3092b1b4df6
nikhil-ds <prasadnikhil25@gmail.com>
2017-10-29 17:08:32 +0000

    Done

--- q04_get_all_sixes_filter/build.py
+++ q04_get_all_sixes_filter/build.py
@@ -1,5 +1,8 @@
 #Default Imports
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 import numpy as np
-
+import pandas as pd
 #Your Solution
+def get_all_sixes_filter():
+    rows = (ipl_matches_array[:,[16]]).flatten()
+    return rows == '6'
