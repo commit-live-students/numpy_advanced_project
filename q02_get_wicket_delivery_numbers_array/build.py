e68ff376637124eb9e5dd8a7c46187e8c8924696
jmcode79 <mukherjee.jeet@gmail.com>
2017-10-30 05:19:36 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,6 +1,14 @@
-#Default Imports
 import numpy as np
+import pandas as pd
 
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 
-#Your Solution
+out = ipl_matches_array[:, [11,20]]
+#matches = np.unique(out[:,0])
+delivery = []
+#matches = np.unique(bats_delivery[:,0])
+def get_wicket_delivery_numbers_array(player):
+    for i,j in out:
+        if j == player:
+            delivery.append(i)
+    return np.array(delivery)
