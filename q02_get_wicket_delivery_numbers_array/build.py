4b4f600d0b9cf1a281db162e8c0dafc6a1a3c342
PawanMathur <pawannupur@gmail.com>
2017-11-01 02:52:39 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -4,3 +4,8 @@ import numpy as np
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 
 #Your Solution
+def get_wicket_delivery_numbers_array(batsman):
+    deliveries_played = ipl_matches_array[ipl_matches_array[:,20] == batsman][:,11]
+    return deliveries_played
+
+#print get_wicket_delivery_numbers_array ('ST Jayasuriya')
