3099e4325711d1834ae007e0427704876fc1336f
PawanMathur <pawannupur@gmail.com>
2017-11-01 04:19:28 +0000

    Done

--- q06_create_runs_series/build.py
+++ q06_create_runs_series/build.py
@@ -4,3 +4,9 @@ import pandas as pd
 
 
 #Your Solution
+def create_runs_series(match_code):
+    match_deliveries = ipl_matches_array[ipl_matches_array[:,0] == match_code]
+    print match_deliveries.shape[0]
+    return pd.Series (data=match_deliveries[:,16],index = match_deliveries[:,11])
+
+#print create_runs_series('392203')
