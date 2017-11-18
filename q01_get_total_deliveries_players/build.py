a27e6c391e5ae9a46871725d8c72186c0798a87f
yuvrajkale87 <yuvraj.kale87@gmail.com>
2017-11-18 05:08:58 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -5,3 +5,8 @@ ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", ski
 
 # Your Solution
 
+def get_total_deliveries_played(batsman):
+    array = ipl_matches_array[:,[11,13]]
+    a_grp = array[array[:] == batsman]
+
+    return np.count_nonzero(a_grp)
