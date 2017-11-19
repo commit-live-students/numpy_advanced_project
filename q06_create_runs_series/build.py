e6b8952f1221a640c4a2eacb884daf62e20b7c28
yuvrajkale87 <yuvraj.kale87@gmail.com>
2017-11-19 06:08:17 +0000

    Done

--- q06_create_runs_series/build.py
+++ q06_create_runs_series/build.py
@@ -4,3 +4,10 @@ import pandas as pd
 
 
 #Your Solution
+def create_runs_series(match_code):
+    array = ipl_matches_array[ipl_matches_array[:,0] ==match_code]
+    #print array
+    delvr_index = array[:,11]
+    runs_column = array[:,16]
+    series = pd.Series(runs_column,index=delvr_index)
+    return series
