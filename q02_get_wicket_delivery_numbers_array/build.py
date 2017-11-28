0def9ccec51b2170a8d3f937958227dae6a00431
chiragpujari <chg.p786@gmail.com>
2017-11-28 04:30:52 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -4,3 +4,10 @@ import numpy as np
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 
 #Your Solution
+def get_wicket_delivery_numbers_array(player):
+    sliced_delivery=ipl_matches_array[:,11:12]
+    sliced_batsman_wicket=ipl_matches_array[:,20:21]
+    data=np.hstack((sliced_delivery,sliced_batsman_wicket))
+    deliveries=data[(np.where(data[:,1:]==player))]
+
+    return deliveries
