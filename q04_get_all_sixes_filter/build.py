d9ced29c9ff24f09715c1e5c714cbee206f097ec
vp999 <patilvish@gmail.com>
2017-11-29 19:59:29 +0000

    Done

--- q04_get_all_sixes_filter/build.py
+++ q04_get_all_sixes_filter/build.py
@@ -3,3 +3,8 @@ from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ip
 import numpy as np
 
 #Your Solution
+def get_all_sixes_filter():
+    ipl_matches_array = np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+    runs= ipl_matches_array[:,16]
+    sixers = (runs == '6')
+    return sixers
