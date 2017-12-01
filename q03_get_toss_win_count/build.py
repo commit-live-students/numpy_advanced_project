483cf5a2242d4ec408d4074e1c2e85121d84d435
palash1512 <palash9515@gmail.com>
2017-12-01 19:25:55 +0000

    Done

--- q03_get_toss_win_count/build.py
+++ q03_get_toss_win_count/build.py
@@ -2,3 +2,9 @@
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 
 #Your Solution
+def get_toss_win_count(team):
+    array=[]
+    for delivery in ipl_matches_array:
+        if(delivery[5]==team):
+            array.append(delivery[0])
+    return len(set(array))
