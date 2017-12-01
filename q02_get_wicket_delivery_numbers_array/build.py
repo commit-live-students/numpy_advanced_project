9c4fa35ad51c47fbeb5a85806067a119d7c89775
swanandbagve <swanandbagve@gmail.com>
2017-12-01 09:06:20 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -4,3 +4,5 @@ import numpy as np
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 
 #Your Solution
+def get_wicket_delivery_numbers_array(player):
+    return ipl_matches_array[ipl_matches_array[:,-3]==player][:,11]
