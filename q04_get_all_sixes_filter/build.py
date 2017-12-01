07cd64e64a0f90c1909f04872632c5629f9d603a
palash1512 <palash9515@gmail.com>
2017-12-01 20:02:14 +0000

    Done

--- q04_get_all_sixes_filter/build.py
+++ q04_get_all_sixes_filter/build.py
@@ -3,3 +3,8 @@ from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ip
 import numpy as np
 
 #Your Solution
+def get_all_sixes_filter():
+    six=[]
+    for item in ipl_matches_array[:,16:17]=='6':
+        six.append(item[0])
+    return six
