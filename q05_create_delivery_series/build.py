d266397340188a2d313bae19888a5b7b2487d505
vp999 <patilvish@gmail.com>
2017-12-02 15:55:16 +0000

    Done

--- q05_create_delivery_series/build.py
+++ q05_create_delivery_series/build.py
@@ -3,3 +3,7 @@ import pandas as pd
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 
 #Your Solution
+from pandas import Series as Series
+
+def create_delivery_series():
+    return  Series( ipl_matches_array[:,11])
--- q06_create_runs_series/build.py
+++ q06_create_runs_series/build.py
@@ -4,3 +4,9 @@ import pandas as pd
 
 
 #Your Solution
+from pandas import Series as Series
+
+def create_runs_series(match_code):
+    matches = ipl_matches_array  [:,(0,11,16)]
+    match= matches[ matches[:,0]==match_code]
+    return Series(data=match[:,2],index=match[:,1])
