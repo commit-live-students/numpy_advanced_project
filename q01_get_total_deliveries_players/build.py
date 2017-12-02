7f8409c5a56668114c32521c270347cb549294ee
swapnilpatil22 <swapnil.patil2205@hotmail.com>
2017-12-02 20:14:25 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -4,4 +4,7 @@ import numpy as np
 ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
 # Your Solution
-
+def get_total_deliveries_played(batsman_name):
+    filteredList= ipl_matches_array[:,13]
+    filteredIndex= (np.where(filteredList==batsman_name)[0])
+    return len(filteredIndex)
