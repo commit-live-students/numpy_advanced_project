a9e41ee8ad1421a8e081707ec085ef1fb2b92af0
honey079sam <honey079sam@gmail.com>
2017-12-04 18:16:34 +0000

    Done

--- q06_create_runs_series/build.py
+++ q06_create_runs_series/build.py
@@ -4,3 +4,11 @@ import pandas as pd
 
 
 #Your Solution
+def create_runs_series(match_code):
+    delivery,runs=[],[]
+    for i in ipl_matches_array[:,[0,11,18]]:
+        if(i[0]==match_code):
+            delivery.append(i[1])
+            runs.append(i[2])
+    s=pd.Series(runs,delivery)
+    return s
