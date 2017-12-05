8d2e2837cc3c523682b1b6e29bc5297f4ed76fd4
brijesh-upadhyay <brijesh_upadhyay@ymail.com>
2017-12-05 19:11:37 +0000

    Done

--- q04_get_all_sixes_filter/build.py
+++ q04_get_all_sixes_filter/build.py
@@ -1,5 +1,16 @@
 #Default Imports
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
+from pandas import Series, DataFrame
+import pandas as pd
 import numpy as np
 
-#Your Solution
+def get_all_sixes_filter():
+    #print ipl_matches_array[:,16:17]
+    #print unique_matches
+    #print ipl_matches_array[:,5]
+    #sixes = np.where(ipl_matches_array[:,16]=='6')
+    #print sixes
+    #return np.unique(ipl_matches_array[rows,0][0]).shape[0]
+    return ipl_matches_array[:,16]=='6'
+
+print get_all_sixes_filter()
