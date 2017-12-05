bcbd820aa05e3a476bc6a3510e2e908af77ec7b9
brijesh-upadhyay <brijesh_upadhyay@ymail.com>
2017-12-05 18:02:26 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,6 +1,14 @@
 #Default Imports
+from pandas import Series, DataFrame
+import pandas as pd
 import numpy as np
-
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 
 #Your Solution
+
+def get_wicket_delivery_numbers_array(player):
+    rows = np.where(ipl_matches_array[:,20] == player)
+    return ipl_matches_array[rows,11][0]
+
+
+print get_wicket_delivery_numbers_array(player='ST Jayasuriya')
