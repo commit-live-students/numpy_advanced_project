27fdfd5eaba92bdb08356e59b766ca77e7c5bdc1
ms10031990 <ms10031990@yahoo.com>
2017-12-05 17:40:11 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,6 +1,15 @@
-#Default Imports
 import numpy as np
 
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 
-#Your Solution
+# Using the loaded data from ipl_matches_array,
+# Scan the "player_out" column -20 and compare the same with player (parameter passed in the function) and
+# append the "delivery" column - 11 value to the list "variable" and return numpy array of variable
+
+variable = list()
+def get_wicket_delivery_numbers_array(player):
+    for element in ipl_matches_array:
+        if element[20] == player:
+            list.append(variable, element[11])
+
+    return np.array(variable)
