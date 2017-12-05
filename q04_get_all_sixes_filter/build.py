19587fb0a459a4d15d941cb4b91cc60c8a02159d
brijesh-upadhyay <brijesh_upadhyay@ymail.com>
2017-12-05 19:27:23 +0000

    Done

--- q04_get_all_sixes_filter/build.py
+++ q04_get_all_sixes_filter/build.py
@@ -5,12 +5,7 @@ import pandas as pd
 import numpy as np
 
 def get_all_sixes_filter():
-    #print ipl_matches_array[:,16:17]
-    #print unique_matches
-    #print ipl_matches_array[:,5]
-    #sixes = np.where(ipl_matches_array[:,16]=='6')
-    #print sixes
-    #return np.unique(ipl_matches_array[rows,0][0]).shape[0]
+
     return ipl_matches_array[:,16]=='6'
 
 print get_all_sixes_filter()
--- q05_create_delivery_series/build.py
+++ q05_create_delivery_series/build.py
@@ -1,5 +1,12 @@
 #Default Imports
+from pandas import Series, DataFrame
 import pandas as pd
+import numpy as np
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 
-#Your Solution
+def create_delivery_series():
+    return pd.Series(ipl_matches_array[:,11])
+    #expected = pd.Series(ipl_matches_array[:, 11])
+    #print expected
+
+print create_delivery_series()
