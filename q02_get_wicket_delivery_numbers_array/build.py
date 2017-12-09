0bd1a66a3d5e8ec0dab92935b232557af0d43a09
tikiabbas <abbas.tiki@gmail.com>
2017-12-09 19:24:57 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,6 +1,10 @@
 #Default Imports
 import numpy as np
-
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
+def get_wicket_delivery_numbers_array (player):
+    #ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+    alldelv = ipl_matches_array[0:len(ipl_matches_array),[11,20]]
+    wicket_delv = alldelv[alldelv[:,1] == player][:,0]
+    return wicket_delv
 
-#Your Solution
+print get_wicket_delivery_numbers_array("ST Jayasuriya")
