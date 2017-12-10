9faf764300d43bce873410f1458ae57fbeac2359
ms10031990 <ms10031990@yahoo.com>
2017-12-10 06:53:49 +0000

    Done

--- q04_get_all_sixes_filter/build.py
+++ q04_get_all_sixes_filter/build.py
@@ -2,4 +2,13 @@
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 import numpy as np
 
-#Your Solution
+# Check the "runs" column if its 6 then append value as 'True' else 'False'
+def get_all_sixes_filter():
+    var=[]
+    for element in ipl_matches_array:
+        if int(element[16])== 6:
+            var.append(True)
+        else:
+            var.append(False)
+    variable =np.array(var)
+    return variable
