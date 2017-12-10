b1fa2956ab1a6d53f5bfa20e1c00b2e996feb97a
gupta1006 <guptagauttam@gmail.com>
2017-12-10 07:26:52 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -2,6 +2,9 @@
 import numpy as np
 
 ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
-
 # Your Solution
-
+def get_total_deliveries_played(batsman):
+    return np.array(
+        [x[0] for x in ipl_matches_array[:, 13:14] if x[0] == batsman]
+        ).size
+get_total_deliveries_played('ST Jayasuriya')
