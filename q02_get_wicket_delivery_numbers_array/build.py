d63c56c5599c647f2a88bd0d87abe17e3d30938c
gupta1006 <guptagauttam@gmail.com>
2017-12-10 10:57:40 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,6 +1,10 @@
+# %load q02_get_wicket_delivery_numbers_array/build.py
 #Default Imports
 import numpy as np
+import pandas as pd
 
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 
-#Your Solution
+def get_wicket_delivery_numbers_array(player):
+    deliveries = ipl_matches_array[:,11:12]
+    return deliveries[ipl_matches_array[:,20:21] == player]
--- q03_get_toss_win_count/build.py
+++ q03_get_toss_win_count/build.py
@@ -1,4 +1,11 @@
+## %load q03_get_toss_win_count/build.py
 #Default Imports
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
-
+import numpy as np
 #Your Solution
+
+def get_toss_win_count(team="Mumbai Indians"):
+    matches = ipl_matches_array[:,0:1]
+    return np.unique(matches[ipl_matches_array [:,5:6] == team]).size
+
+get_toss_win_count()
