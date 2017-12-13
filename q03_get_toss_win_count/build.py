e98d08de59dfaa24434cba328d85aaa6909c2784
pratikkotian04 <pratikkotian04@gmail.com>
2017-12-13 04:37:27 +0000

    Done

--- q03_get_toss_win_count/build.py
+++ q03_get_toss_win_count/build.py
@@ -1,4 +1,9 @@
 #Default Imports
+import numpy as np
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 
 #Your Solution
+def get_toss_win_count(team):
+    data=ipl_matches_array[:,0]
+    count=np.unique(data[ipl_matches_array[:,5]==team]).size
+    return count
