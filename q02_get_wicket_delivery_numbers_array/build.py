7bd4cad94cf9b57137dca080c0c1a06439dac788
nazirwa <nazirmail@gmail.com>
2017-12-14 18:56:24 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -4,3 +4,10 @@ import numpy as np
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 
 #Your Solution
+def get_wicket_delivery_numbers_array(player):
+    player_out_delivery = ipl_matches_array[ipl_matches_array[:,20] == player][:,11]
+    return player_out_delivery
+
+#print 'STJ:{0}'.format(get_wicket_delivery_numbers_array('ST Jayasuriya'))
+#print 'Yuvraj:{0}'.format(get_wicket_delivery_numbers_array('Yuvraj Singh'))
+#print '10dulkar:{0}'.format(get_wicket_delivery_numbers_array('SR Tendulkar'))
