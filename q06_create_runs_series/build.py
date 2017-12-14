682883ba513e4b5edb0fd7425b3e16480668b088
gupta1006 <guptagauttam@gmail.com>
2017-12-14 06:45:28 +0000

    Done

--- q06_create_runs_series/build.py
+++ q06_create_runs_series/build.py
@@ -1,6 +1,11 @@
+# %load q06_create_runs_series/build.py
 #Default Imports
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 import pandas as pd
+from pandas import Series
 
-
-#Your Solution
+def create_runs_series(match_code):
+    match = ipl_matches_array[:,0] == match_code
+    deliveries = Series(ipl_matches_array[:,11][match])
+    runs = Series(ipl_matches_array[:,16][match])
+    return Series (runs.values,deliveries)
