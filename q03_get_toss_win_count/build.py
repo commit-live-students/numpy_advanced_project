626f8c18d40c8800123d829d2fb044aa3f48ab27
harshavachhani <harshavachhani@gmail.com>
2017-12-16 17:58:24 +0000

    Done

--- q03_get_toss_win_count/build.py
+++ q03_get_toss_win_count/build.py
@@ -4,4 +4,7 @@ ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", ski
 
 
 #Your Solution
-
+def get_toss_win_count(team):
+    sliced_toss_winner=ipl_matches_array[:,5:6]
+    count=len(sliced_toss_winner[(np.where((sliced_toss_winner==team,1)))])
+    return count
