4ad486ccd5c6976d2e91d6812d7e732f19cd3aa1
snmjack <snmjack@gmail.com>
2017-12-17 04:01:38 +0000

    Done

--- q06_create_runs_series/build.py
+++ q06_create_runs_series/build.py
@@ -4,3 +4,17 @@ import pandas as pd
 
 
 #Your Solution
+
+def create_runs_series(match_code):
+
+    matchcode = ipl_matches_array[:,0]
+    delivery = pd.Series(ipl_matches_array[:,11])
+    run = pd.Series(ipl_matches_array[:,16], index = delivery)
+    runs = run[matchcode == match_code]
+    #print delivery
+    #print runs
+
+    #columns = ['delivery','match_code','runs']
+    #data = matchcode[match_code].pd.Series(runs, delivery)
+
+    return runs
