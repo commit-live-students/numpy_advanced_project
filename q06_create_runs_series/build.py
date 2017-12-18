e87b7b114db91c09a756c8ec0402775a513fd3b3
nazirwa <nazirmail@gmail.com>
2017-12-18 18:42:07 +0000

    Done

--- q06_create_runs_series/build.py
+++ q06_create_runs_series/build.py
@@ -4,3 +4,9 @@ import pandas as pd
 
 
 #Your Solution
+import numpy as np
+def create_runs_series(match):
+    ipl_filtered = ipl_matches_array[ipl_matches_array[:,0].astype(np.int64) == 392203]
+    ser = pd.Series(data=ipl_filtered[:,16], index=ipl_filtered[:,11])
+    return ser
+#print create_runs_series(392203)
