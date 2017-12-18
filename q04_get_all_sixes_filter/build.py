013a961f53cd7d5202a350742649ed1c1e853965
nazirwa <nazirmail@gmail.com>
2017-12-18 18:16:53 +0000

    Done

--- q04_get_all_sixes_filter/build.py
+++ q04_get_all_sixes_filter/build.py
@@ -3,3 +3,9 @@ from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ip
 import numpy as np
 
 #Your Solution
+import pandas as pd
+#Your Solution
+def get_all_sixes_filter():
+    return ipl_matches_array[:,16].astype(np.int16) == 6
+
+#print get_all_sixes_filter()
