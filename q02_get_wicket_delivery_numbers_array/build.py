39b9098c165ef0ce094f2248b9db94c5a8515a22
vaithyanathanramanathan <vaithy31@gmail.com>
2017-12-18 16:01:33 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -3,5 +3,5 @@ import numpy as np
 
 ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
-#Your Solution
-
+def get_wicket_delivery_numbers_array(player):
+	return np.array([elm[11] for elm in ipl_matches_array if elm[13] == player and elm[20] <> ''])
