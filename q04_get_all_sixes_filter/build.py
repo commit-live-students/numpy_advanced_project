6b3e296705927afeabd2a2eecaf255359eba475a
abkedar <anupbhande@gmail.com>
2017-12-19 05:31:26 +0000

    Done

--- q04_get_all_sixes_filter/build.py
+++ q04_get_all_sixes_filter/build.py
@@ -1,5 +1,15 @@
+from pandas import Series, DataFrame
+import pandas as pd
+import numpy as np
 #Default Imports
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
-import numpy as np
+
+#Default Imports
+
 
 #Your Solution
+#Your Solution
+def get_all_sixes_filter():
+    runs = (ipl_matches_array[:, 16].astype(np.int16) == 6)
+    return (runs)
+print get_all_sixes_filter()
