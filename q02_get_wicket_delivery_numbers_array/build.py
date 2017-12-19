e02225c15da9edb847b0b1a9763e168ded8d49a5
abkedar <anupbhande@gmail.com>
2017-12-19 04:37:04 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -2,13 +2,18 @@
 import numpy as np
 import pandas as pd
 from pandas import Series, DataFrame
-#from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
-ipl_matches_array = np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", delimiter=",")
+from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
+#ipl_matches_array = np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50",skip_header = 1, delimiter=",")
 #Your Solution
-def get_wicket_delivery_numbers_array():
-    players = DataFrame(ipl_matches_array[:, :])
+def get_wicket_delivery_numbers_array(player_out):
+    #player1 = DataFrame(ipl_matches_array[:, 19:20])
+    rows = np.where(ipl_matches_array[:, 20:21] == player_out)
+    #delivery = DataFrame(ipl_matches_array[:, 11:12])
+
+    return ipl_matches_array[rows, 11][0]
 
 
 
-    print players
-print get_wicket_delivery_numbers_array()
+
+
+print get_wicket_delivery_numbers_array(player_out = "ST Jayasuriya")
