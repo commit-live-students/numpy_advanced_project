f2381196e49a27f814da63c9ff38480905e5b862
swapnilpatil22 <swapnil.patil2205@hotmail.com>
2017-12-19 11:38:18 +0000

    Done

--- q05_create_delivery_series/build.py
+++ q05_create_delivery_series/build.py
@@ -1,5 +1,7 @@
 #Default Imports
 import pandas as pd
 from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
-
+import numpy as np
 #Your Solution
+def create_delivery_series():
+    return pd.Series(ipl_matches_array[:, 11])
