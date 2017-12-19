6d2a707d3fa0a472f64ae1cfaa40971f6dc82464
abkedar <anupbhande@gmail.com>
2017-12-19 04:26:51 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -1,7 +1,15 @@
-# Default imports
+# Default import
 import numpy as np
-
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
-
+import pandas as pd
+from pandas import Series, DataFrame
+ipl_matches_array = np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 # Your Solution
+def get_total_deliveries_played(batsman = "SR Tendulkar"):
+    batsman1 = DataFrame(ipl_matches_array[:, 13:14])
+    count1 = DataFrame(batsman1)
+    count1.columns = ['name']
+    count_deliveries = count1.name.value_counts()[batsman]
+    #print type(count_deliveries)
+    return count_deliveries
 
+print get_total_deliveries_played(batsman = "SR Tendulkar")
--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,6 +1,14 @@
 #Default Imports
 import numpy as np
-
-from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
-
+import pandas as pd
+from pandas import Series, DataFrame
+#from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
+ipl_matches_array = np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", delimiter=",")
 #Your Solution
+def get_wicket_delivery_numbers_array():
+    players = DataFrame(ipl_matches_array[:, :])
+
+
+
+    print players
+print get_wicket_delivery_numbers_array()
