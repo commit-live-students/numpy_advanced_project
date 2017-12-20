0cca7eed7dace915bc9de0d359bfc291ea2b7063
JohnCenaaa <ashlin+2@greyatom.com>
2017-12-20 05:40:13 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,6 +1,3 @@
-#Default Imports
-import numpy as np
+# %load q02_get_wicket_delivery_numbers_array/build.py
 
-from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 
-#Your Solution
