675702f8293f1df7a69acea3fc662f2bd160a732
Mayank211 <mayank2saxena@gmail.com>
2017-12-21 15:04:11 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,3 +1,4 @@
+# %load q02_get_wicket_delivery_numbers_array/build.py
 #Default Imports
 import numpy as np
 
@@ -5,3 +6,9 @@ ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", ski
 
 #Your Solution
 
+def get_wicket_delivery_numbers_array(player):
+    delivery=ipl_matches_array[:,11:12]
+    wicket=ipl_matches_array[:,20:21]
+    data=np.hstack((delivery,wicket))
+    deliveries=data[(np.where(data[:,1:]==player))]
+    return deliveries
