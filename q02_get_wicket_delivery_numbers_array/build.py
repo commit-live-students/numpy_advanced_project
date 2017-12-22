b53b17ae68a915ff648e045bac404c7cb1beb042
shaikferozgithub <shaikferozmail@gmail.com>
2017-12-22 13:57:22 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,7 +1,15 @@
+# %load q02_get_wicket_delivery_numbers_array/build.py
 #Default Imports
 import numpy as np
-
+from numpy import array
 ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+def get_total_deliveries_played(batsman):
+    cnt=[]
+    for matches in ipl_matches_array:
+        if matches[-3] == batsman:
+            cnt.append(matches[11])
+    cnt1=array(cnt)
+    return cnt1
 
-#Your Solution
-
+res=get_total_deliveries_played('ST Jayasuriya')
+print res
