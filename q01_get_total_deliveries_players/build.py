ac5843fc32b5d429ac93ef42b1cd10c0b4ff60ff
Sankarnivas <sankarnivas.s@gmail.com>
2017-12-26 12:38:26 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -1,7 +1,17 @@
+# %load q01_get_total_deliveries_players/build.py
 # Default imports
 import numpy as np
+import pandas as pd
+from pandas import DataFrame
 
 ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
-
 # Your Solution
+def get_total_deliveries_played(batsman):
+    total = 0
+    for i in ipl_matches_array:
+        if batsman==i[13]:
+            total = total + 1
+    return total
 
+
+tendulkar_total = get_total_deliveries_played('SR Tendulkar')
