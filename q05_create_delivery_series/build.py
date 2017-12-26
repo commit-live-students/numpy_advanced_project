83a1d8cfcd90fc205ae1c7718a4b995f934cb708
Sankarnivas <sankarnivas.s@gmail.com>
2017-12-26 14:20:36 +0000

    Done

--- q05_create_delivery_series/build.py
+++ q05_create_delivery_series/build.py
@@ -1,7 +1,16 @@
+# %load q05_create_delivery_series/build.py
 #Default Imports
 import pandas as pd
 import numpy as np
+from pandas import Series
 ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
 #Your Solution
+def create_delivery_series():
+    deliveries = []
+    for i in ipl_matches_array:
+        deliveries.append(i[11])
+    actual = Series(deliveries)
+    return actual
 
+actual = create_delivery_series()
