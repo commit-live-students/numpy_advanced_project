1db1bbb9fe1b6cf89a84316f6514220fd8c58944
pasugithub <pasu.rm@gmail.com>
2017-12-29 13:09:10 +0000

    Done

--- q03_get_toss_win_count/build.py
+++ q03_get_toss_win_count/build.py
@@ -2,6 +2,11 @@
 import numpy as np
 ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
-
+import pandas as pd
 #Your Solution
-
+def get_toss_win_count(team):
+    base_df = pd.DataFrame(ipl_matches_array)
+    my_df = base_df[[0,5]]
+    toss_win_teams = my_df.groupby([5])[0].nunique()
+    return int(toss_win_teams[team])
+print get_toss_win_count('Mumbai Indians')
