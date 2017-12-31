f54172d6e313be6503f2fee079d61861c5ee35d0
aarthikat <aarthika.t@gmail.com>
2017-12-31 13:04:57 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -5,3 +5,8 @@ ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", ski
 
 # Your Solution
 
+def get_total_deliveries_played(batsman):
+    val=ipl_matches_array[:,13]
+    res=np.count_nonzero(val[:]==batsman)
+    return res
+get_total_deliveries_played('ST Tendulkar')
--- q03_get_toss_win_count/build.py
+++ q03_get_toss_win_count/build.py
@@ -5,3 +5,13 @@ ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", ski
 
 #Your Solution
 
+def get_toss_win_count(team):
+    cnt1=[]
+    for matches in ipl_matches_array:
+        if matches[5]==team:
+            cnt1.append(matches[0])
+    #rint cnt1
+    #print set(cnt1)
+    return len(set(cnt1))
+
+get_toss_win_count('Mumbai Indians')
