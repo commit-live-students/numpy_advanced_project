3b0d9476f8b542e4c69982a7a199b30c39e42529
santoshmayekar <santosh.mayekar@outlook.com>
2017-12-31 09:10:15 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,7 +1,10 @@
-#Default Imports
 import numpy as np
 
 ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
-#Your Solution
-
+def get_wicket_delivery_numbers_array(player):
+    a=ipl_matches_array[:,20]
+    itemindex = np.where(a==player)
+    ilst = list(itemindex)
+    return ipl_matches_array[ilst,11]
+    
