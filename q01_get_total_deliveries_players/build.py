e5e5e8df730df7e97a86c889fb9eb4b1d7c5b202
r-baldawa <r.baldawa17@gmail.com>
2018-01-03 15:55:37 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -1,7 +1,13 @@
 # Default imports
 import numpy as np
+import pandas as pd
+from pandas import Series, DataFrame
 
 ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
 # Your Solution
 
+def get_total_deliveries_played(batsman):
+    df_ipl_matches = DataFrame(ipl_matches_array)
+    df_batsman_count = df_ipl_matches[13].value_counts()
+    return int(df_batsman_count[batsman])
