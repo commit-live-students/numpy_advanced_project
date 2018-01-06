74adf900c08df5baaf1413d6a51f456aa42485e9
rohangudsurkar17 <james.clayton.ron@gmail.com>
2018-01-06 08:56:44 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -5,3 +5,10 @@ ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", ski
 
 #Your Solution
 
+def get_wicket_delivery_numbers_array(player):
+    delivery_nos = []
+    for i in ipl_matches_array[:,[11,20]]:
+         if i[1] == player:
+            delivery_nos.append(i[0])
+
+    return delivery_nos
