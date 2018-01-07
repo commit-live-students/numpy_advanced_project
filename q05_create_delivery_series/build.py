81475feab1bcc8bd2376a0c3ebcddb9c1b10cc7f
oldflame <dhavl32@gmail.com>
2018-01-07 11:03:27 +0000

    Done

--- q05_create_delivery_series/build.py
+++ q05_create_delivery_series/build.py
@@ -1,7 +1,10 @@
+# %load q05_create_delivery_series/build.py
 #Default Imports
 import pandas as pd
 import numpy as np
 ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
 #Your Solution
-
+def create_delivery_series():
+    x = pd.Series(ipl_matches_array[:,11])
+    return x
