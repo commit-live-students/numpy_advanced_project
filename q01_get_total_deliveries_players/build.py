873e8ff3bfe1963c4062c13cf8e1e64769370eec
Rajachellan <raja.june15@gmail.com>
2018-01-09 04:21:18 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -1,7 +1,6 @@
 # Default imports
 import numpy as np
 
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
-
-# Your Solution
-
+ipl_matches_array = np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+def get_total_deliveries_played(player):
+    return np.sum(np.where(ipl_matches_array[0:,13]==player,1,0))# Your Solution
