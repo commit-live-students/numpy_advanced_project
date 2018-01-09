7abbaf7327f266467323df22abc78e9904db7d4f
Rajachellan <raja.june15@gmail.com>
2018-01-09 04:25:57 +0000

    Done

--- q03_get_toss_win_count/build.py
+++ q03_get_toss_win_count/build.py
@@ -1,7 +1,6 @@
-#Default Imports
 import numpy as np
+
 ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
-
-#Your Solution
-
+def get_toss_win_count(teamname):
+    return len(np.unique([elm[0] for elm in ipl_matches_array if elm[5] == teamname]))
