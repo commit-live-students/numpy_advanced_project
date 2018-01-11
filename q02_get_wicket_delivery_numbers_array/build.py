037cdef5d80a0c6db30f5b400f1ff013b2c4da8c
RohinSequeira <sequeira.rohin@gmail.com>
2018-01-11 01:55:16 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -4,4 +4,8 @@ import numpy as np
 ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
 #Your Solution
-
+def get_wicket_delivery_numbers_array(batsman):
+    ball = ipl_matches_array[:,11:12]
+    wicket = ipl_matches_array[:,20:21]
+    pair = np.hstack((ball, wicket))
+    return pair[(np.where(pair[:,1:]==batsman))]
