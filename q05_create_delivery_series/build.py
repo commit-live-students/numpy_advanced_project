80727a33441e6b68e6d7fbed330ea791a3d54a82
brockLesnar37 <ashlin+37@greyatom.com>
2018-01-20 06:02:25 +0000

    Done

--- q05_create_delivery_series/build.py
+++ q05_create_delivery_series/build.py
@@ -1,7 +1,9 @@
+
 #Default Imports
 import pandas as pd
 import numpy as np
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
 
 #Your Solution
+
 
