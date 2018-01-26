f0f8da058cb570998ab6d7c37e1522bec5f6f993
siddhichandan <siddhichandan@gmail.com>
2018-01-26 15:37:13 +0000

    Done

--- q03_get_toss_win_count/build.py
+++ q03_get_toss_win_count/build.py
@@ -4,4 +4,6 @@ ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", ski
 
 
 #Your Solution
-
+def get_toss_win_count(teamname):
+    n1 = np.where(ipl_matches_array[:,5] == teamname)
+    return np.unique(ipl_matches_array[n1,0]).size
