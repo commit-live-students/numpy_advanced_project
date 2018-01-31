340471dcd99157341177fac5941346eb77cdaa93
axc888 <jha.ashish43@gmail.com>
2018-01-31 17:58:18 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -4,4 +4,12 @@ import numpy as np
 ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
 # Your Solution
-
+def get_total_deliveries_played(batsman):
+    unique_batsman=np.unique(ipl_matches_array[:,13],return_counts=True)
+    found_batsman = np.where(unique_batsman[0]==batsman)
+    deliveries_batted=unique_batsman[1][found_batsman]
+    #print(unique_batsman[0])
+    #print(found_batsman)
+    #print(deliveries_batted)
+    return deliveries_batted
+#print(get_total_deliveries_played('SR Tendulkar'))
