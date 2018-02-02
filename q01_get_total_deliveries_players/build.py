379e57df8117d3445aa2e6d8b69ca21e0f55b807
viishnu712 <viishnu712@gmail.com>
2018-02-02 12:19:16 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -1,7 +1,13 @@
 # Default imports
 import numpy as np
 
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=0, delimiter=",")
 
 # Your Solution
-
+import pandas as pd
+df= pd.DataFrame(ipl_matches_array)
+df.columns = df.loc[0,:]
+dfnew = df.drop(0)
+def get_total_deliveries_played(batsman):
+    serBatsman = (dfnew['batsman'].value_counts())
+    return serBatsman[batsman]
