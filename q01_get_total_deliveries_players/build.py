42faab3884e066251cef46bd48f0945e424d834a
Srishtisawla <sawlasrishti@gmail.com>
2018-02-03 16:54:19 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -1,7 +1,9 @@
 # Default imports
 import numpy as np
 
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv",skip_header=1, dtype="|S50",  delimiter=",")
 
-# Your Solution
-
+def get_total_deliveries_played(batsman):
+    bat,count = np.unique(ipl_matches_array[:,13],return_counts=True)
+    a = dict(zip(bat,count))
+    return a[batsman]
