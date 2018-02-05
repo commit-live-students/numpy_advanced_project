9b33889c5db717646f51827754967e30f0e3a285
shravanighatnekar <joshipoonam0303@gmail.com>
2018-02-05 16:55:03 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -1,7 +1,19 @@
 # Default imports
 import numpy as np
+import pandas as pd
+from pandas import Series, DataFrame
 
 ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
 # Your Solution
+def get_total_deliveries_played(batsman='ST Jayasuriya'):
+    df = DataFrame(ipl_matches_array[:,13], columns=['Batsman'])
 
+    df['count'] = 1
+
+    on_bat = df.pivot_table('count', aggfunc=np.sum, index='Batsman')
+    Jay = on_bat.loc[batsman].item()
+
+    return Jay
+
+print get_total_deliveries_played('ST Jayasuriya')
