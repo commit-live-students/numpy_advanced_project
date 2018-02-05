0fbef7865dc88bed810166bc0d60b02bbf350dd0
sacvijai <sacvijai@gmail.com>
2018-02-05 18:58:38 +0000

    Done

--- q06_create_runs_series/build.py
+++ q06_create_runs_series/build.py
@@ -1,7 +1,10 @@
 #Default Imports
 import pandas as pd
 import numpy as np
+from pandas import Series
 ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
 #Your Solution
-
+def create_runs_series(match_code):
+    ipl_match_code = ipl_matches_array[ipl_matches_array[0:,0]== match_code]
+    return Series(ipl_match_code[0:,16],index=ipl_match_code[0:,11])
