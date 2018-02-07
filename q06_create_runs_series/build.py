f469e671813a0da121caf5dce45016126d83a502
shravanighatnekar <joshipoonam0303@gmail.com>
2018-02-07 10:05:15 +0000

    Done

--- q06_create_runs_series/build.py
+++ q06_create_runs_series/build.py
@@ -4,4 +4,12 @@ import numpy as np
 ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
 #Your Solution
+def create_runs_series(match_code='392203'):
+    df = pd.DataFrame(ipl_matches_array[:,[0,11,16]], columns=['match','deliveries', 'runs'])
+    l = df.loc[df['match'] == match_code, ['deliveries', 'runs']]
 
+    s = pd.Series(l['runs'].values, index=l['deliveries'])
+
+    return s
+
+print create_runs_series('392203')
