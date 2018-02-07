8c5ca993803ba3672c3404079a143278f6a77f57
sohelahmadkhan <sohelahmadkhan000@gmail.com>
2018-02-07 06:11:40 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -1,7 +1,16 @@
 # Default imports
+
 import numpy as np
 
 ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
-# Your Solution
+def get_total_deliveries_played(batsman):
+    bats_col = ipl_matches_array[:,13]
+    unq = np.unique(bats_col,return_counts = True)
+    plr = np.where(unq[0] == batsman)
 
+    deliv = int(unq[1][plr])
+
+    return deliv
+
+print get_total_deliveries_played('SR Tendulkar')
