dafac48b5a15e36673eb162732936633be5deb1c
shravanighatnekar <joshipoonam0303@gmail.com>
2018-02-07 09:06:27 +0000

    Done

--- q04_get_all_sixes_filter/build.py
+++ q04_get_all_sixes_filter/build.py
@@ -4,4 +4,9 @@ ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", ski
 
 
 #Your Solution
+def get_all_sixes_filter():
+    ary = (ipl_matches_array[:, 16].astype(np.int16) == 6)
 
+
+    return ary
+print get_all_sixes_filter()
