78219384762312de96f5c85ef4bf3f967fd20a99
Sharad30 <sharad30.ss@gmail.com>
2018-02-08 09:49:10 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -3,5 +3,12 @@ import numpy as np
 
 ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
-# Your Solution
+def get_total_deliveries_played(batsman):
 
+    batsman_array = ipl_matches_array[:,13]
+    unique, counts = np.unique(batsman_array, return_counts=True)
+    batsman_list = zip(unique,counts)
+
+    for ele in batsman_list:
+        if ele[0] == 'SR Tendulkar':
+            return ele[1]
