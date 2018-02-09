01eea2f32ab759e1dfd99fc48e931b04c4c32d85
Sharad30 <sharad30.ss@gmail.com>
2018-02-09 15:04:40 +0000

    Done

--- q03_get_toss_win_count/build.py
+++ q03_get_toss_win_count/build.py
@@ -1,7 +1,19 @@
 #Default Imports
 import numpy as np
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", delimiter=",", dtype="|S50", skip_header=1)
 
+def get_toss_win_count(team):
 
-#Your Solution
+    match_id = ipl_matches_array[:,[0,5]]
+    unique_match = np.unique(match_id, axis = 0)
+    print(unique_match)
+    #unique_toss = np.unique(toss_array)
+    #toss_list = zip(unique_toss,toss_count)
+    #team_count = zip(unique_team,team_count)
 
+    '''for ele in team_array:
+
+        team_id,toss = ele
+        if toss == team'''
+
+get_toss_win_count('Mumbai Indians')
--- q04_get_all_sixes_filter/build.py
+++ q04_get_all_sixes_filter/build.py
@@ -2,6 +2,9 @@
 import numpy as np
 ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
+def get_all_sixes_filter():
 
-#Your Solution
+    runs_array = ipl_matches_array[:,16]
+    sixes_array = runs_array == '6'
 
+    return sixes_array
