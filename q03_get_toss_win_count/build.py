b22a232e97a08686811a63e06632653d6854abed
HerambD <heramb.dharmadhikari@gmail.com>
2018-02-13 19:33:01 +0000

    Done

--- q03_get_toss_win_count/build.py
+++ q03_get_toss_win_count/build.py
@@ -4,4 +4,11 @@ ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", ski
 
 
 #Your Solution
+def get_toss_win_count(team):
+    dic = {}
+    toss_count = 0
+    for i in ipl_matches_array[:,[0,5]]:
+        if i[0] not in dic:
+            dic[i[0]] = i[1]  # Make a dic of unique matches and toss winner team
 
+    return len([x for x in dic.values() if x == team])
