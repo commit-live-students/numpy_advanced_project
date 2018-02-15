0a46d395e26c89fdeaee77f4d861163c74431497
Ketan10994 <katcee10@gmail.com>
2018-02-15 08:54:37 +0000

    Done

--- q04_get_all_sixes_filter/build.py
+++ q04_get_all_sixes_filter/build.py
@@ -3,5 +3,10 @@ import numpy as np
 ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
 
-#Your Solution
+def get_all_sixes_filter():
+    a = ipl_matches_array
+    b = a[:,16]
+    c=  [b == '6']
+    return c
 
+print get_all_sixes_filter()
