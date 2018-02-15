10a84e39f91ad7f04f10382dadd8b7f707d0e74f
guptaaman92 <guptaaman92@gmail.com>
2018-02-15 02:23:25 +0000

    Done

--- q03_get_toss_win_count/build.py
+++ q03_get_toss_win_count/build.py
@@ -4,4 +4,11 @@ ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", ski
 
 
 #Your Solution
-
+def get_toss_win_count(team):
+    match_team = ipl_matches_array[:,[0,5]]
+    unique_matches = np.vstack({tuple(e) for e in match_team})
+    unique , counts = np.unique(unique_matches, return_counts= True)
+    team_toss_win = dict(zip(unique,counts))
+    tosses_won =  team_toss_win[team]
+    return tosses_won
+get_toss_win_count('Mumbai Indians')    
