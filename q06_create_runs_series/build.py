b220877e86f16f3b51b9f79874f1a0384d74101c
PBPatil <theprasadpatil@gmail.com>
2018-02-16 11:12:12 +0000

    Done

--- q06_create_runs_series/build.py
+++ q06_create_runs_series/build.py
@@ -4,4 +4,7 @@ import numpy as np
 ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
 #Your Solution
-
+def create_runs_series(match_code):
+    deliv_col =ipl_matches_array[:,11][ipl_matches_array[:,0]==match_code]
+    run_scored =ipl_matches_array[:,16][ipl_matches_array[:,0]==match_code]
+    return pd.Series(run_scored,index=deliv_col)
