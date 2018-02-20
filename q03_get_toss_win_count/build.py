9c8d8b20c6f3f1a32c08f9f4d9ce5961db9d87b7
seema200 <singh.seema200@gmail.com>
2018-02-20 09:00:30 +0000

    Done

--- q03_get_toss_win_count/build.py
+++ q03_get_toss_win_count/build.py
@@ -4,4 +4,10 @@ ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", ski
 
 
 #Your Solution
-
+team='Mumbai Indians'
+def get_toss_win_count(team):
+    match=ipl_matches_array[:,0]
+    tosses_won=ipl_matches_array[:,7]
+    match_tosses_won=np.column_stack((match,tosses_won))
+    count_win=len(np.unique(match_tosses_won[np.where(match_tosses_won[:,1:]==team)]))
+    return count_win
