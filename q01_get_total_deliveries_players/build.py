77937b0459070700309392c0322c336350baf324
sagarrawale <sagar.rawale3@gmail.com>
2018-02-23 12:45:49 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -1,7 +1,16 @@
 # Default imports
 import numpy as np
-
+import pandas as pd
 ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
-# Your Solution
 
+
+batsman = 'ST Jayasuriya'
+def get_total_deliveries_played(batsman):
+    batsman1 = ipl_matches_array[: , 13]
+    a = batsman1 == batsman
+    total_deliveries = len(ipl_matches_array[a])
+    return total_deliveries
+
+get_total_deliveries_played(batsman)
+# Your Solution
