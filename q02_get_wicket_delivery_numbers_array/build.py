d1760cf13bc58098d6355f9bfcb768f11662214e
sagarrawale <sagar.rawale3@gmail.com>
2018-02-23 12:53:05 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -4,4 +4,11 @@ import numpy as np
 ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
 #Your Solution
+player = 'ST Jayasurya '
 
+def get_wicket_delivery_numbers_array(player):
+    wicket = ipl_matches_array[:,20] == player
+    wicket_deliveries = ipl_matches_array[wicket]
+    return wicket_deliveries[:,11]
+
+get_wicket_delivery_numbers_array(player)
