f451379cf831419062c6991144e886560e3c29bc
CharulataShelar <charulatashelar@gmail.com>
2018-02-24 17:55:59 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -4,4 +4,20 @@ import numpy as np
 ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
 #Your Solution
+def get_wicket_delivery_numbers_array(player):
+    #l = []
+    #for row in ipl_matches_array:
+    #    if row[-3] == player:
+    #        delivery = float(row[-12])
+    #       l.append(delivery)
 
+    #all_delivery = np.array(l)
+    #return all_delivery
+
+    wicket_delivery = ipl_matches_array[:,[-3,-12]]
+    #print newdata
+    player_delivery = wicket_delivery[wicket_delivery[:, 0] == player]
+    all_delivery = player_delivery[:, 1]
+    return all_delivery
+    
+get_wicket_delivery_numbers_array('ST Jayasuriya')
--- q03_get_toss_win_count/build.py
+++ q03_get_toss_win_count/build.py
@@ -4,4 +4,9 @@ ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", ski
 
 
 #Your Solution
+def get_toss_win_count(team):
+    team_match_code = ipl_matches_array[ipl_matches_array[:,5] == team][:,0]
+    match_code = np.unique(team_match_code)
+    return match_code.shape[0]
 
+get_toss_win_count('Mumbai Indians')
