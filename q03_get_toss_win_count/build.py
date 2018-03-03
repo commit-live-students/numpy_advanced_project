e60f952985eeda51748faa1e01ad4dd20d86a807
RuchitaSave <save.ruchita4@gmail.com>
2018-03-03 21:16:46 +0000

    Done

--- q03_get_toss_win_count/build.py
+++ q03_get_toss_win_count/build.py
@@ -2,6 +2,9 @@
 import numpy as np
 ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
+def get_toss_win_count(team):
+    toss_count = ipl_matches_array[:,5] == team
+    team_won_array = ipl_matches_array[toss_count]
+    return np.unique(team_won_array[:,0]).size
 
-#Your Solution
-
+#get_toss_win_count('Mumbai Indians')
