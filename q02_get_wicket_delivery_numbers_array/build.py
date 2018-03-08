04d01042768666272f9b864eb9eb45f7297fc5d9
sejal-varu <sejalvaru@gmail.com>
2018-03-08 12:11:41 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -4,4 +4,6 @@ import numpy as np
 ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
 #Your Solution
-
+def get_wicket_delivery_numbers_array(player):
+    played_out = ipl_matches_array[:,[11,20]]
+    return played_out[played_out[:,1] == player][:,0]
