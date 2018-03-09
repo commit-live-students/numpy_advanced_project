df45d5cea93e7405ab4967b1be49e78d1359a8af
ga-ashlin-dev <ashlin+78@greyatom.com>
2018-03-09 06:03:57 +0000

    Done

--- q06_create_runs_series/build.py
+++ q06_create_runs_series/build.py
@@ -1,7 +1,11 @@
+
 #Default Imports
+from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 import pandas as pd
-import numpy as np
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+
 
 #Your Solution
+def create_runs_series(match_code):
+    match = ipl_matches_array[ipl_matches_array[:, 0] == match_code]
+    return pd.Series(match[:, 16], index=match[:, 11])
 
