3acd0e8257b830ef4052413b2e7a5fd76e35a5c1
ankitalalwani <lalwaniankita20@gmail.com>
2018-04-23 04:37:10 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -1,7 +1,23 @@
+# %load q01_get_total_deliveries_players/build.py
 # Default imports
+
 import numpy as np
 
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
-
+ipl_matches_array = np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
+count = 0
 # Your Solution
+
+def get_total_deliveries_played(batsman):
+    
+    
+    ipl_batsman = ipl_matches_array[0:,13].astype(str) == batsman
+    return sum(ipl_batsman)
+
+get_total_deliveries_played('SR Tendulkar')
+       
+
+
+
+
+
 
--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,7 +1,19 @@
+# %load q02_get_wicket_delivery_numbers_array/build.py
 #Default Imports
 import numpy as np
 
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
 
 #Your Solution
+def get_wicket_delivery_numbers_array(player):
+    
+    player_name = ipl_matches_array.astype(str)
+    return ipl_matches_array[player_name[:,-3] == player][:,-12]
+        
+    
+    
+    
+get_wicket_delivery_numbers_array('ST Jayasuriya')
+
+
 
