94afbf411f87ddbd59de93718bf1a2d092513ecd
ankitalalwani <lalwaniankita20@gmail.com>
2018-04-23 04:39:44 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -8,9 +8,9 @@ ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', ski
 def get_wicket_delivery_numbers_array(player):
     
     player_name = ipl_matches_array.astype(str)
-    return ipl_matches_array[player_name[:,-3] == player][:,-12]
+    return ipl_matches_array[player_name[:,-3] == player][:,-12].astype(str)
         
-    
+     
     
     
 get_wicket_delivery_numbers_array('ST Jayasuriya')
