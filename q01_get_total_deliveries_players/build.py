28f2ca88a2591c89569d3fec211d94cc5fd94cf4
swapcy <email2swap@gmail.com>
2018-04-23 11:33:14 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -1,7 +1,15 @@
+# %load q01_get_total_deliveries_players/build.py
 # Default imports
 import numpy as np
-
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
-
+import pandas as pd
+#ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
 # Your Solution
+
+def get_total_deliveries_played(batsman='MEK Hussey'):
+    ipl_match = pd.read_csv('data/ipl_matches_small.csv')
+    ball_faced = ipl_match['batsman'].value_counts()
+    
+    return ball_faced[batsman]
+
+get_total_deliveries_played('SR Tendulkar')
 
