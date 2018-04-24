be8605dc2dbf89b65f18b3274f7c95770dfa9376
HardikaB <hardi.2607@gmail.com>
2018-04-24 09:53:18 +0000

    Done

--- q03_get_toss_win_count/build.py
+++ q03_get_toss_win_count/build.py
@@ -1,7 +1,11 @@
+# %load q03_get_toss_win_count/build.py
 #Default Imports
+from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 import numpy as np
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
-
-
 #Your Solution
+def get_toss_win_count(team):
+    ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
+    arr=ipl_matches_array[:,0][ipl_matches_array[:,5].astype(np.str)==team]
+    arr=np.unique(arr)
+    return np.count_nonzero(arr)
 
