187375a64cfede2e2a702522eb031a5198dd1fa1
rushi0109 <rushi0109@gmail.com>
2018-04-25 18:14:01 +0000

    Done

--- q04_get_all_sixes_filter/build.py
+++ q04_get_all_sixes_filter/build.py
@@ -1,7 +1,17 @@
+# %load q04_get_all_sixes_filter/build.py
 #Default Imports
 import numpy as np
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
 
 
 #Your Solution
+
+def get_all_sixes_filter():
+    runArray = ipl_matches_array[:,16].astype(int)
+    #unique , count = np.unique(runArray,return_counts = True ) 
+    #dictRuns = dict(zip(unique,count))
+    boolArray = runArray == 6
+    print(boolArray)
+    return boolArray
+get_all_sixes_filter()
 
