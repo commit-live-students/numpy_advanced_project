52b9523a42ac9d6335a8bd1d0d46f1b1072a219b
kishangooner <kishansk26@gmail.com>
2018-04-28 15:07:44 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -1,7 +1,15 @@
+# %load q01_get_total_deliveries_players/build.py
 # Default imports
 import numpy as np
 
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
 
 # Your Solution
+def get_total_deliveries_played (batsman):
+    deliveriesFaced=0
+    for data in ipl_matches_array:
+        if (batsman in str(data[-10])):
+            deliveriesFaced=deliveriesFaced+1
+    return(deliveriesFaced)
+
 
