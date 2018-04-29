ea64a0919dfbd327ead48d43cc624414977266e8
kishangooner <kishansk26@gmail.com>
2018-04-29 14:58:16 +0000

    Done

--- q04_get_all_sixes_filter/build.py
+++ q04_get_all_sixes_filter/build.py
@@ -1,7 +1,12 @@
+# %load q04_get_all_sixes_filter/build.py
 #Default Imports
 import numpy as np
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
 
-
-#Your Solution
+def get_all_sixes_filter():
+    #array=np.array(6==data[-7].astype(np.int16))
+    for data in ipl_matches_array:
+        array=np.array(data[-7])
+        return(array.astype(np.int16)==6)
+     
 
--- q05_create_delivery_series/build.py
+++ q05_create_delivery_series/build.py
@@ -1,7 +1,14 @@
+# %load q05_create_delivery_series/build.py
 #Default Imports
 import pandas as pd
 import numpy as np
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
 
+def create_delivery_series():
+    series=pd.Series(ipl_matches_array[0:,-12])
+    return(series)
+    
 #Your Solution
+#create_delivery_series()
+
 
