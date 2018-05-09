08e7dc7a7518b31ed9a004a9092d40907507109c
bharadwajk9 <utkb.k9@gmail.com>
2018-05-09 17:34:04 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -1,7 +1,15 @@
-# Default imports
+from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
 import numpy as np
+import pandas as pd
+path  = 'data/ipl_matches_small.csv'
 
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+def get_total_deliveries_played(batsman):
+    ipl_data = np.genfromtxt(path, dtype='|S50', skip_header=1, delimiter=',')
+    deliveries = ipl_data[:,13].astype(str)
+    deliveries = deliveries[deliveries==batsman]
+    return deliveries.size
 
-# Your Solution
+get_total_deliveries_played('SR Tendulkar')
+
+
 
