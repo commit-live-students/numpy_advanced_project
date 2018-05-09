030ec90e1b2a2fe21caf0b39836f2e1b4bab5ed2
Sumeshks29 <sumeshks29@gmail.com>
2018-05-09 10:05:17 +0000

    Done

--- q04_get_all_sixes_filter/build.py
+++ q04_get_all_sixes_filter/build.py
@@ -1,7 +1,17 @@
+# %load q04_get_all_sixes_filter/build.py
 #Default Imports
 import numpy as np
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
 
 
 #Your Solution
+def get_all_sixes_filter():
+    team_records = ipl_matches_array[:, 16]
+    
+    team_filter = (team_records ==b'6')
+    print(team_filter)
+    return team_filter
+get_all_sixes_filter()
+                                     
+                                     
 
