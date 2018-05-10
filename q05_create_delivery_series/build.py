a7ef0b9cc3d1d42f5c3ba4da4ebd10d3c6e4d025
Sumeshks29 <sumeshks29@gmail.com>
2018-05-10 10:36:00 +0000

    Done

--- q05_create_delivery_series/build.py
+++ q05_create_delivery_series/build.py
@@ -1,7 +1,15 @@
+# %load q05_create_delivery_series/build.py
 #Default Imports
 import pandas as pd
 import numpy as np
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
-
-#Your Solution
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv',dtype= str, skip_header=1, delimiter=',')
+batsman = 'ST Jayasuriya'
+ #Your Solution
+def create_delivery_series(batsman):
+    delivery_filter = (ipl_matches_array[:,13] == batsman)
+    delivery_series = ipl_matches_array[delivery_filter]
+    delivery_series = pd.Series(delivery_series[:,11])
+    return delivery_series[:,11]
+create_delivery_series(batsman)
+    
 
--- q06_create_runs_series/build.py
+++ q06_create_runs_series/build.py
@@ -1,7 +1,17 @@
+# %load q06_create_runs_series/build.py
 #Default Imports
 import pandas as pd
 import numpy as np
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', delimiter=',',dtype=str)
+match_code = '392203'
+# #Your Solution
+def create_runs_series(match_code):
+    ipl_filter=(ipl_matches_array[:,0]==match_code)
+    ipl_matches=ipl_matches_array[ipl_filter]
+    matches_index = ipl_matches[:,11] 
+    matches_runs = ipl_matches[:,16]
+    return pd.Series(data=matches_runs,index=matches_index)
+create_runs_series(match_code)
+    
 
-#Your Solution
 
