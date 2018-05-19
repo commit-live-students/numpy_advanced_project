55ccb090590f54c2a13014766b52a0ffda257d92
prasannajayashankar <prasanna.jay@gmail.com>
2018-05-19 04:20:23 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,7 +1,20 @@
+# %load q02_get_wicket_delivery_numbers_array/build.py
 #Default Imports
 import numpy as np
-
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
-
+import pandas as pd
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
+def get_wicket_delivery_numbers_array(player):
+    data_df=pd.read_csv('data/ipl_matches_small.csv')
+    delivery_data_out=data_df[['batsman','player_out','delivery']][data_df['player_out']==player]
+    return delivery_data_out['delivery']
+    
 #Your Solution
+
+
+data_df=pd.read_csv('data/ipl_matches_small.csv')
+data_df.info()
+data_df[['batsman','player_out','delivery']][data_df['player_out']=='SR Tendulkar']
+delivery_data_out=data_df[['batsman','player_out','delivery']][data_df['player_out']=='SR Tendulkar']
+delivery_data_out['delivery']
+
 
