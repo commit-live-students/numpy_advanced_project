ed2747da542b5fe3946ae29cd1ef37ff9d6c20d8
Anand-Radhey <anand.radhey@gmail.com>
2018-05-22 16:18:02 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,7 +1,17 @@
+# %load q02_get_wicket_delivery_numbers_array/build.py
 #Default Imports
 import numpy as np
+import pandas as pd
 
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
 #Your Solution
+def get_wicket_delivery_numbers_array(player):
+    ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv',dtype='|S50', skip_header=1, delimiter=',')
+    deliveries = ipl_matches_array[:,11]
+    condn = ipl_matches_array[:,20].astype(str) == str(player)
+    result = deliveries[condn].astype(str)
+    
+    return np.array(result)
+
+get_wicket_delivery_numbers_array('ST Jayasuriya')
 
