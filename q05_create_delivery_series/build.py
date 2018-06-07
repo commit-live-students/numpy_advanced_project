96bae399e2f59c1dc4086f2f51e588a1e6a995a0
Sumeshks29 <sumeshks29@gmail.com>
2018-06-07 12:15:41 +0000

    Done

--- q05_create_delivery_series/build.py
+++ q05_create_delivery_series/build.py
@@ -1,15 +1,10 @@
-# %load q05_create_delivery_series/build.py
 #Default Imports
 import pandas as pd
-import numpy as np
-ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv',dtype= str, skip_header=1, delimiter=',')
-batsman = 'ST Jayasuriya'
- #Your Solution
-def create_delivery_series(batsman):
-    delivery_filter = (ipl_matches_array[:,13] == batsman)
-    delivery_series = ipl_matches_array[delivery_filter]
-    delivery_series = pd.Series(delivery_series[:,11])
-    return delivery_series[:,11]
-create_delivery_series(batsman)
+from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
+
+#Your Solution
+def create_delivery_series():
+    return pd.Series(ipl_matches_array[:, 11])
+
     
 
