32b07c970b9dd15511e8c65430361e7a1c89d455
Chinmay Chopade <cchopade@gmail.com>
2018-07-17 12:36:23 +0530

    Update test_q02_get_wicket_delivery_numbers_array.py

--- q02_get_wicket_delivery_numbers_array/tests/test_q02_get_wicket_delivery_numbers_array.py
+++ q02_get_wicket_delivery_numbers_array/tests/test_q02_get_wicket_delivery_numbers_array.py
@@ -5,4 +5,4 @@ from ..build import get_wicket_delivery_numbers_array
 class TestGet_wicket_delivery_numbers_array(TestCase):
     def test_get_wicket_delivery_numbers_array(self):
         deliveries = get_wicket_delivery_numbers_array(b'ST Jayasuriya')
-        self.assertTrue(np.all(deliveries == np.array([3.2, 14.4, 1.4])), 'Expected Value does not match with the given values' )
+        self.assertTrue(np.all(deliveries == np.array([b'3.2', b'14.4',b'1.4'])), 'Expected Value does not match with the given values' )
