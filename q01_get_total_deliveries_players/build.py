997e3b5c9ae0569caddfafe27ccc010fc496675e
nalinmshah <nalinmshah@gmail.com>
2018-07-21 08:25:50 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -1,7 +1,19 @@
-# Default imports
-import numpy as np
+import pandas as pd
 
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+str_path='data/ipl_matches_small.csv'
 
-# Your Solution
+def read_ipl_data_csv(path):
+    pd_series= pd.read_csv(path,  delimiter=',')
+    return pd_series
+
+def get_total_deliveries_played(batsman):
+    batsman = 'SR Tendulkar'
+    return series_ipl['batsman'].value_counts()[batsman]
+
+
+series_ipl = read_ipl_data_csv(str_path)
+int_batsman_deliveries = get_total_deliveries_played('fdsfdsfsdf fdfsdfds')
+print('int_batsman_deliveries : ',int_batsman_deliveries)
+
+
 
