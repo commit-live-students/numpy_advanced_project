a5dc684b561996f8e4c34a6005a1f622c921a4f9
Tusharsharma86 <tusharsharma86@gmail.com>
2018-07-22 15:23:01 +0000

    Done

--- q06_create_runs_series/build.py
+++ q06_create_runs_series/build.py
@@ -4,15 +4,15 @@ import pandas as pd
 import numpy as np
 ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', skip_header=1, delimiter=',')
 
-#Your Solution
+#create the funtion 
 def create_runs_series(match_code):
-    
+    ''' Function to create series od runs with deliveres as columns'''
     ipl_matches_array1 = ipl_matches_array[ipl_matches_array[:,0]==392203]
     data_pd = pd.Series(ipl_matches_array1[:,-7].astype('int32'), index=ipl_matches_array1[:,11])
     return data_pd
 
 
-
+#Call function 
 create_runs_series(392203)
 
 
