d9db65b464fd3bf238f43db1bf9045a1b4693db0
sahusaurabh65 <sahusaurabh65@gmail.com>
2018-07-24 22:05:59 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -16,8 +16,11 @@ import numpy as np
 ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
 
 def get_wicket_delivery_numbers_array(player):
+    #making list for get_wicket_delivery_numbers_array
     out=[]
+    #getting match info
     info = ipl_matches_array[:,[11,20]]
+    #find the name of player in[:,[ 11,20]]
     for x in info:
         if x[1] == player:
             out.append(x[0])
