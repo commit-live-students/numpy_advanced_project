f2aa4df06fabbbecff8f07d4bed693619820bc5f
kaushik1975 <kaushik1975@gmail.com>
2018-07-28 06:01:57 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,14 +1,19 @@
 # %load q02_get_wicket_delivery_numbers_array/build.py
 #Default Imports
 import numpy as np
-import pandas as pd
 
 ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
-batsman = 'ST Jayasuriya'
+batsman = b'ST Jayasuriya'
 #Your Solution
 def get_wicket_delivery_numbers_array(batsman):
+    '''
     df = pd.read_csv('data/ipl_matches_small.csv')
     wicket_delivery_numbers_array = df.loc[df['player_out']==batsman]
-    return wicket_delivery_numbers_array['delivery'].values
+    return np.array(wicket_delivery_numbers_array['delivery'])
+    '''
+    arr = ipl_matches_array[:,[11,20]]
+    condition = (arr[:,1]==batsman)
+    arr_1 = arr[condition]
+    return arr_1[:,0]
 
 
