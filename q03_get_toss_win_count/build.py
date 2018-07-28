9dad6de17754dcfc6163df08791751b01d24e1bc
kaushik1975 <kaushik1975@gmail.com>
2018-07-28 06:16:23 +0000

    Done

--- q03_get_toss_win_count/build.py
+++ q03_get_toss_win_count/build.py
@@ -3,14 +3,19 @@
 import numpy as np
 import pandas as pd
 ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
-Team_Name = 'Mumbai Indians'
+Team_Name = b'Mumbai Indians'
 
 #Your Solution
 def get_toss_win_count(Team_Name):
+    '''
     df = pd.read_csv('data/ipl_matches_small.csv')
     df1 = df[df['toss_winner']==Team_Name]['match_code'].drop_duplicates()
     return len(df1.index)
-Team_Name = 'Mumbai Indians'
-get_toss_win_count(Team_Name)
+    '''
+    arr = np.unique(ipl_matches_array[:,[0,5]], axis=0)
+    condition = (arr[:,1]==Team_Name)
+    arr_1 = arr[condition]
+    return len(arr_1)
+
 
 
