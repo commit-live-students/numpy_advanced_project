55376f555810f6c05277716e9bac469125f6255c
rianboy18 <pranavr95.pn@gmail.com>
2018-07-30 10:33:11 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -1,7 +1,25 @@
+# %load q01_get_total_deliveries_players/build.py
 # Default imports
 import numpy as np
 
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
-
 # Your Solution
+def get_total_deliveries_played(batsman):
+    ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
+    a=ipl_matches_array
+    p=[]
+    q=[]
+    for i in range(len(a)):
+        b=tuple((a[i][13],a[i][11]))
+        p.append(b)
+    for i in (p):
+            if i[0]==batsman:
+                q.append(float(i[1]))
+    return len(q)
+    
+c=get_total_deliveries_played(b'SR Tendulkar')
+
+c
+
+
+
 
--- q05_create_delivery_series/build.py
+++ q05_create_delivery_series/build.py
@@ -1,7 +1,17 @@
+# %load q05_create_delivery_series/build.py
 #Default Imports
 import pandas as pd
 import numpy as np
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
 #Your Solution
+def create_delivery_series():
+    ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
+    a=pd.Series(ipl_matches_array[:,11])
+    return type(a)
+    
+
+
+b=create_delivery_series()
+b
+
 
