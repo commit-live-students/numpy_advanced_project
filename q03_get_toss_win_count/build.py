552f753afa5e83493409499fe14b3ad77a49bfba
Akashdesarda <desardaakash@gmail.com>
2018-08-07 13:42:01 +0000

    Done

--- q03_get_toss_win_count/build.py
+++ q03_get_toss_win_count/build.py
@@ -1,7 +1,16 @@
+# %load q03_get_toss_win_count/build.py
 #Default Imports
 import numpy as np
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
-
+import pandas as pd
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
+df = pd.read_csv('data/ipl_matches_small.csv')
 
 #Your Solution
+def get_toss_win_count(team):
+    a = np.array(ipl_matches_array[:,5]) 
+    b= list(a)
+    return int(b.count(b'Mumbai Indians')/(236))
+get_toss_win_count('Mumbai Indians')
+type(get_toss_win_count('Mumbai Indians'))
+
 
