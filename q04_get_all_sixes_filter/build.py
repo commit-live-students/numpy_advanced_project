4ba18c75966c0ae62a0e66dc27c823619fce6332
raahuul <er.rahul.v27@gmail.com>
2018-08-08 06:21:41 +0000

    Done

--- q04_get_all_sixes_filter/build.py
+++ q04_get_all_sixes_filter/build.py
@@ -1,7 +1,14 @@
+# %load q04_get_all_sixes_filter/build.py
 #Default Imports
 import numpy as np
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+
 
 
 #Your Solution
+def get_all_sixes_filter():
+    ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
+    return np.array([True if x.astype(np.int16)==6 else False for x in ipl_matches_array[0:,16]])
+
+
+
 
