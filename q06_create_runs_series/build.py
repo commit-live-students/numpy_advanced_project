b95e1caf8f3cbd7089e7c7f29fa60e823b0d02b5
delirium0712 <ritwiky7@gmail.com>
2018-08-09 12:39:15 +0000

    Done

--- q06_create_runs_series/build.py
+++ q06_create_runs_series/build.py
@@ -1,7 +1,22 @@
+# %load q06_create_runs_series/build.py
 #Default Imports
 import pandas as pd
 import numpy as np
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
-
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
+ipldf=pd.read_csv('data/ipl_matches_small.csv',dtype='|S50')
 #Your Solution
+
+def create_runs_series(match_code):
+    dseries = ipldf.loc[ipldf['match_code'] == match_code]
+    dseries = ipldf[['runs','delivery']]
+    variable = dseries.set_index('delivery')
+    return variable.runs
+
+
+    
+    
+
+
+
+
 
