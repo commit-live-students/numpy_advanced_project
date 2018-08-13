c83c8539a961223166b87c727355fb1dcfdc4e5a
ankutty <akutty@deloitte.com>
2018-08-13 09:02:19 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,7 +1,19 @@
+# %load q02_get_wicket_delivery_numbers_array/build.py
 #Default Imports
 import numpy as np
 
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
 
 #Your Solution
+def get_wicket_delivery_numbers_array(player):
+    wicket_filter  = (ipl_matches_array[:,20] == player)
+    wickets_arr = ipl_matches_array[wicket_filter]
+    return wickets_arr[:, 11]
+    
+    
+print(get_wicket_delivery_numbers_array('Harbhajan Singh'))
+    
+
+
+
 
