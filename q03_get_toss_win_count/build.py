904767bfb5333ac8fa4c5c49060a37988c2aa944
KamalShillongwala <qamaldas@gmail.com>
2018-08-18 04:28:57 +0000

    Done

--- q03_get_toss_win_count/build.py
+++ q03_get_toss_win_count/build.py
@@ -5,15 +5,9 @@ ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', ski
 
 
 #Your Solution
-def get_toss_win_count(team = 'Mumbai Indians'):
-    toss_won = 0
-    for x in ipl_matches_array:
-        inning = int(x[10])
-        delivery = float(x[11])
-        toss_winner = str(x[5])
-        if inning == 1 and delivery == 0.1 and team in toss_winner:
-            toss_won = toss_won + 1
-    toss_won = int(toss_won)
-    return toss_won
+def get_toss_win_count(team = b'Mumbai Indians'):
+    toss_won = ipl_matches_array[ipl_matches_array[:,5] == team]
+    matches = len(set(toss_won[:,0]))
+    return matches
 
 
