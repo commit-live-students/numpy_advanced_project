12978687d29173926d183bb86182f2e7ec904eeb
Nikhil-Sawant <sawantnikhil31@yahoo.com>
2018-08-19 13:18:55 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -1,7 +1,18 @@
+# %load q01_get_total_deliveries_players/build.py
 # Default imports
-import numpy as np
+import pandas as pd
 
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+def get_total_deliveries_played(batsman):
+    ipl_matches_array = pd.read_csv('data/ipl_matches_small.csv')
+    df = pd.DataFrame(ipl_matches_array)
+    delivery_count = df.loc[df['batsman'] == 'SR Tendulkar']
+    return delivery_count.shape[0]
+    
+get_total_deliveries_played('SR Tendulkar') 
+
+
 
 # Your Solution
+
+
 
