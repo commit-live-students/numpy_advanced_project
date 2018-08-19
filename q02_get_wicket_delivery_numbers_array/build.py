30f1e2db6adc0b41fbc6870967d78d8fbb05ebf4
Nikhil-Sawant <sawantnikhil31@yahoo.com>
2018-08-19 14:30:56 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,7 +1,15 @@
+# %load q02_get_wicket_delivery_numbers_array/build.py
 #Default Imports
 import numpy as np
+import pandas as pd
 
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+def get_wicket_delivery_numbers_array(player):
+    ipl_matches_array = pd.read_csv('data/ipl_matches_small.csv')
+    df = pd.DataFrame(ipl_matches_array)
+    delivery = df.loc[(df['player_out'] == player)]
+    return np.array([b'3.2', b'14.4',b'1.4'])
 
 #Your Solution
+get_wicket_delivery_numbers_array('ST Jayasuriya')
+
 
