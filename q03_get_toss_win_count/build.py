05fe89ced45638375ee91b48571a06ef58e0b5cc
apurvi02 <apurvij@gmail.com>
2018-08-20 17:46:55 +0000

    Done

--- q03_get_toss_win_count/build.py
+++ q03_get_toss_win_count/build.py
@@ -1,7 +1,14 @@
+# %load q03_get_toss_win_count/build.py
 #Default Imports
 import numpy as np
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
-
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
 
 #Your Solution
+def get_toss_win_count(team):
+    a=ipl_matches_array[ipl_matches_array[:,5].astype(str)==team]
+    b=np.unique(a[:,0])
+    return len(b)
+get_toss_win_count('Mumbai Indians')
+
+
 
--- q04_get_all_sixes_filter/build.py
+++ q04_get_all_sixes_filter/build.py
@@ -1,7 +1,13 @@
+# %load q04_get_all_sixes_filter/build.py
 #Default Imports
 import numpy as np
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
-
-
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
+l=list(enumerate(ipl_matches_array[0,:]))
+print(l)
 #Your Solution
+def get_all_sixes_filter():
+    a=np.array([ipl_matches_array[:,16].astype(np.int16)==6],dtype=bool)
+    return (a)
+get_all_sixes_filter()
+
 
