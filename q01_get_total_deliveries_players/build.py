fb4a8b35a06c460de8532de50d246ec3e735089a
ramakrishnasonakam <ramakrishnasonakam@gmail.com>
2018-08-22 09:06:02 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -1,7 +1,22 @@
+# %load q01_get_total_deliveries_players/build
 # Default imports
 import numpy as np
-
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
-
+import pandas as pd
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
+path = ipl_matches_array
 # Your Solution
+def get_total_deliveries_played(path):
+    count = 0
+    ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
+    batsman = ipl_matches_array[:,-10].astype(np.str)
+    batsman_series = pd.Series(batsman)
+    
+    for l in batsman_series:
+        if l == 'SR Tendulkar':
+            count+=1
+    print(count)
+    return count
+get_total_deliveries_played(path)
+
+
 
