1b7f71d1f325ff92919cce811882247e2ffa938a
avnishxlr8 <avnish.vish95@gmail.com>
2018-09-02 16:24:36 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,7 +1,23 @@
+# %load q02_get_wicket_delivery_numbers_array/build.py
 #Default Imports
 import numpy as np
 
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
 
+def get_wicket_delivery_numbers_array(player):
+    data = ipl_matches_array
+    ball = []
+    for row in data:
+        if(row[20] == player):
+            print(row[11])
+            ball.append(row[11])
+    return type(np.array(ball))
+
+
+#get_wicket_delivery_numbers_array(b'ST Jayasuriya')
+    
 #Your Solution
+
+
+
 
