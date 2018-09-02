a56be879a5016901577652427e345232dfafa4b5
avnishxlr8 <avnish.vish95@gmail.com>
2018-09-02 15:03:36 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -1,7 +1,23 @@
+# %load q01_get_total_deliveries_players/build.py
 # Default imports
 import numpy as np
 
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
 
-# Your Solution
+def get_total_deliveries_played(batsman):
+    # Your Solution
+    data = ipl_matches_array
+    ball = []
+    for row in data:
+        if(row[13] == batsman):
+            #print(type(row[11]))
+            ball.append(row[11])
+    #print(len(ball))
+    return len(ball)
+
+
+#get_total_deliveries_played(b'SR Tendulkar')
+
+          
+
 
