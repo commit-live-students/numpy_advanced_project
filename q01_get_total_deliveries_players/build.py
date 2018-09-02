ffc63a7db1f172fe10936c34fe338720770d0645
prodigy803 <pushkaraj803@gmail.com>
2018-09-02 08:52:43 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -1,7 +1,20 @@
+# %load q01_get_total_deliveries_players/build.py
 # Default imports
 import numpy as np
 
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+ipl_matches_array = np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
 
 # Your Solution
+def get_total_deliveries_played(batsman):
+    total_rows = ipl_matches_array.shape[0]
+    count =0
+    for i in range(0,total_rows):
+        if ipl_matches_array[i][13] == batsman:#.encode(encoding='UTF-8'):
+            count = count + 1
+    return count
+#get_total_deliveries_played(b'ST Jayasuriya')
+#
+
+#my_ipl_matches_array
+
 
