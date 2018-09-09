16c3323f1934f11fc57adcf585c9788624b71145
sreelekhaWoonna <sreelekha4877@gmail.com>
2018-09-09 04:42:05 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -6,11 +6,13 @@ ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', ski
 
 #Your Solution
 def get_wicket_delivery_numbers_array (player_out):
-    return np.array(ipl_matches_array[:, [11, 20]][np.where(ipl_matches_array[:, [11, 20]][:,1] == player_out)][:,:1])
+    return np.array(ipl_matches_array[:, [11, 20]][np.where(ipl_matches_array[:, [11, 20]][:,1] == player_out)][:,:1]).flatten().astype('|S4')
+player_out = b'ST Jayasuriya'
 
-
-ipl_matches_array[:, [11, 20]][np.where(ipl_matches_array[:, [11, 20]][:,1] == b'ST Jayasuriya')][:,:1]
-
+np.array(ipl_matches_array[:, [11, 20]][np.where(ipl_matches_array[:, [11, 20]][:,1] == player_out)][:,:1]).astype('|S4')
+np.array([b'3.2', b'14.4',b'1.4'])
+deliveries = get_wicket_delivery_numbers_array(b'ST Jayasuriya')
+np.all(deliveries == np.array([b'3.2', b'14.4',b'1.4']))
 
 
 
