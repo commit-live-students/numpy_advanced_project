1de5af2fb4d2ca3b2e31f5108a114aef0e8f4db1
Abhimanyu22 <link2manyu@hotmail.com>
2018-09-09 06:15:17 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,7 +1,15 @@
-#Default Imports
 import numpy as np
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
 
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+def get_wicket_delivery_numbers_array(player_name):
+    arr = ipl_matches_array[ipl_matches_array[:,20].astype('|S50') == player_name]
+    arr1 = arr[:,11]
+    
+    return arr1
 
-#Your Solution
+
+    
+
+
+
 
