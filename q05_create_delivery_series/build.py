5ef3e4365f14300eb79a4a3cf29d8543a05cb7ab
savans7 <connecttosavan@gmail.com>
2018-09-12 02:35:15 +0000

    Done

--- q05_create_delivery_series/build.py
+++ q05_create_delivery_series/build.py
@@ -1,7 +1,17 @@
-#Default Imports
 import pandas as pd
 import numpy as np
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
-#Your Solution
+def create_delivery_series() :
+   # team=team.decode('ASCII')
+    ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
+
+    #ipl_matches_array =pd.read_csv('data/ipl_matches_small.csv')
+    s1=pd.Series(ipl_matches_array[:,11])
+    #expected_filter = (ipl_matches_array[:, 16] == 6)
+    print(type(s1))
+    #&(df['toss_decision']!='bat')
+    return s1
+
+print(create_delivery_series())
+
 
