cb3a9d537d5cf822cd37dd6e6942c785f407b3cb
dhananjay93 <dhananjay.hawal@gmail.com>
2018-09-12 10:52:07 +0000

    Done

--- q03_get_toss_win_count/build.py
+++ q03_get_toss_win_count/build.py
@@ -1,7 +1,16 @@
+# %load q03_get_toss_win_count/build.py
 #Default Imports
 import numpy as np
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
-
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
 
 #Your Solution
+def get_toss_win_count(team = b'Mumbai Indians' ):
+        var1 = np.count_nonzero(ipl_matches_array[:,5] == team)
+        return var1
+    
+    
+
+
+
+
 
--- q04_get_all_sixes_filter/build.py
+++ q04_get_all_sixes_filter/build.py
@@ -1,7 +1,18 @@
+# %load q04_get_all_sixes_filter/build.py
 #Default Imports
 import numpy as np
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
 
 
 #Your Solution
+def get_all_sixes_filter():
+    runs1 = (ipl_matches_array[:,-7] == b'6' )
+    return runs1
+    
+    
+
+
+
+
+
 
