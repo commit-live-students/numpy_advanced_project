07cda9179a1f47873f374bbb4ec55be51f7b8c71
sunilhariharan <sunilhariharan@gmail.com>
2018-09-16 18:33:47 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -7,15 +7,14 @@ ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', ski
 #Your Solution
 def get_wicket_delivery_numbers_array(player):
     
+    arr=ipl_matches_array[:,[11,20]]
+    
+    arr1=arr[~np.isnull(arr).any(axis=1)]
+    
+    arr2=arr1[:,0]
     
     
-    a=ipl_matches_array[~np.isnan(ipl_matches_array).any(axis=1)]
-    
-    a1=a[:,11]
-    
-    
-    
-    return a1
+    return arr2
     
 
 
