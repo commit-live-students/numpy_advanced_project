c95d7f8b596d77e1f021c8d0ae1fa5694f4c0e2d
vivekshingate <vivekshingate@ymail.com>
2018-09-16 12:22:05 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,7 +1,22 @@
+# %load q02_get_wicket_delivery_numbers_array/build.py
 #Default Imports
 import numpy as np
 
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=0, delimiter=',')
 
 #Your Solution
+def get_wicket_delivery_numbers_array(player):
+    delivery_list = np.array([])
+    for i,v in enumerate(ipl_matches_array):
+        if ipl_matches_array[i][-3] == player.encode():
+            #print(ipl_matches_array[i][11],ipl_matches_array[i][20])
+            delivery_list = np.append(delivery_list,ipl_matches_array[i][11])
+    return delivery_list
+
+get_wicket_delivery_numbers_array('SR Tendulkar')
+
+
+a=np.array([])
+print(a)
+
 
