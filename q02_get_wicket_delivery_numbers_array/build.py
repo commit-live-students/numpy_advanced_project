c3433cceabbd313381e2fb57c9ac66f842252a09
sunilhariharan <sunilhariharan@gmail.com>
2018-09-16 15:06:32 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,7 +1,23 @@
+# %load q02_get_wicket_delivery_numbers_array/build.py
 #Default Imports
 import numpy as np
 
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
 
 #Your Solution
+def get_wicket_delivery_numbers_array(player):
+    
+    
+    
+    a=ipl_matches_array[~np.isnan(ipl_matches_array).any(axis=1)]
+    
+    a1=a[:,11]
+    
+    
+    
+    return a1
+    
+
+
+
 
--- q03_get_toss_win_count/build.py
+++ q03_get_toss_win_count/build.py
@@ -1,7 +1,20 @@
+# %load q03_get_toss_win_count/build.py
 #Default Imports
 import numpy as np
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
 
 
 #Your Solution
+def get_toss_win_count(team):
+    
+    unique, counts = np.unique(ipl_matches_array[:,5], return_counts=True)
+    d=dict(zip(unique, counts))
+    
+    toss=d['Mumbai Indians']
+    
+    return toss
+    
+
+
+
 
