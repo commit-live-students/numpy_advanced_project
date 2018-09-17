08c8e82baac4b89516fb05cfe0e1af073cf91831
sunilhariharan <sunilhariharan@gmail.com>
2018-09-17 19:12:34 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -9,12 +9,11 @@ def get_wicket_delivery_numbers_array(player):
     
     arr=ipl_matches_array[:,[11,20]]
     
-    arr1=arr[~np.isnull(arr).any(axis=1)]
-    
-    arr2=arr1[:,0]
     
     
-    return arr2
+    arr1=arr[list(np.where(arr[:,1]==b'ST Jayasuriya')),0]
+    
+    return arr1
     
 
 
