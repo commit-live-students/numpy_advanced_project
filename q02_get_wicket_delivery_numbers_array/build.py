61bf229677bdc4f467e09bbc8fae02873d3d52bd
Ajpalav <ajit.palav@gmail.com>
2018-09-21 19:29:50 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,7 +1,20 @@
-#Default Imports
+#%load q02_get_wicket_delivery_numbers_array/build.py
 import numpy as np
 
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+path='./data/ipl_matches_small.csv'
 
-#Your Solution
+def get_wicket_delivery_numbers_array(player):
+    
+
+    arr=np.genfromtxt(path,delimiter=',',dtype='|S20',skip_header=1)
+    arr1=(arr[:,20]==player)
+    #print(arr[:,11:20])
+    #print(arr[:,11][arr1])
+    
+    return arr[:,11][arr1]
+
+
+get_wicket_delivery_numbers_array(b'Harbhajan Singh')
+
+
 
