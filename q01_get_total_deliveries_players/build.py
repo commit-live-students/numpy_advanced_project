fcfe1121567b1e3aa158e91d9ac8e149f57e17ee
Lavi1920 <lavitha1920@gmail.com>
2018-09-27 15:20:33 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -2,16 +2,14 @@
 # Default imports
 import numpy as np
 ipl_matches_array = np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
-def get_total_deliveries_played(batsman):
-    tr = ipl_matches_array.shape[0] #total rows in csv file
-#print(total_rows)
-    count =0
-    for i in range(0,tr):
-        if ipl_matches_array[i][13] == batsman:
-            count = count + 1
-    return count
-
-
+def get_wicket_delivery_numbers_array(player):
+    wick_deliveries = []
+    rows = ipl_matches_array.shape[0]
+    
+    for i in range(0,rows):
+        if ipl_matches_array[i][20] == player:
+            wick_deliveries.append(ipl_matches_array[i][11])
+    return np.array(wick_deliveries)
 
 
 
--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,7 +1,27 @@
+# %load q02_get_wicket_delivery_numbers_array/build.py
 #Default Imports
 import numpy as np
 
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
 
 #Your Solution
+
+def get_wicket_delivery_numbers_array(player):
+    w = []
+    
+    rows = ipl_matches_array.shape[0]
+    
+    for i in range(0,rows):
+        if ipl_matches_array[i][20] == player:
+            w.append(ipl_matches_array[i][11])
+    
+    return np.array(w)
+
+
+
+
+
+
+
+
 
