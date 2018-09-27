a05f2cdb24f3c378083a46c57bba00a4414ab178
sannidh <soniknitr@gmail.com>
2018-09-27 16:53:37 +0000

    Done

--- q05_create_delivery_series/build.py
+++ q05_create_delivery_series/build.py
@@ -7,12 +7,8 @@ ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', ski
 def create_delivery_series():
     deliveries=pd.Series(ipl_matches_array[:,11])
     return deliveries
-import pandas as pd
-import numpy as np
-ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
-balls_faced=ipl_matches_array[ipl_matches_array[:,13]==b'SC Ganguly']
-deliveries=pd.Series(ipl_matches_array[:,11])
-type(deliveries)
+
+
 
 
 
