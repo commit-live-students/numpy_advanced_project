680d0ce4a4cb87ba1e69a3da4acbe698dedbf8f7
Sandesh373 <dyavanapellisandesh373@gmail.com>
2018-09-27 19:28:48 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,7 +1,15 @@
+# %load q02_get_wicket_delivery_numbers_array/build.py
 #Default Imports
 import numpy as np
 
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
 
-#Your Solution
+def get_wicket_delivery_numbers_array(batsman):
+    delivery = ipl_matches_array[:,11] # delievery column which has to be given in op is considered as main dataset
+    playerout = ipl_matches_array[:,20]==batsman # [create filter: batsman] accesssing the column playerout 
+    return delivery[playerout] #apply the filter in the main data(deliveries to get the values)
+
+
+get_wicket_delivery_numbers_array(b'SR Tendulkar')
+
 
