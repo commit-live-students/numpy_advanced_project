789728b8f3b3b8c4de253ffff8cf961a8197837a
sannidh <soniknitr@gmail.com>
2018-09-27 16:51:34 +0000

    Done

--- q04_get_all_sixes_filter/build.py
+++ q04_get_all_sixes_filter/build.py
@@ -6,7 +6,6 @@ ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', ski
 def get_all_sixes_filter():
     sixers=ipl_matches_array[:,-7].astype(np.int64)==6
     return sixers
-sixers=ipl_matches_array[:,16].astype(np.int64)==6
-sixers
+
 
 
