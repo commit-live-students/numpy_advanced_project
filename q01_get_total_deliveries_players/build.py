e21b66d37bd47bd53e634ce3fc9d106902cdafaa
sannidh <soniknitr@gmail.com>
2018-09-27 16:45:29 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -8,7 +8,7 @@ ipl_matches_array =np.genfromtxt('./data/ipl_matches_small.csv', dtype='|S50', s
 def get_total_deliveries_played(batsman):
     deliveries=ipl_matches_array[ipl_matches_array[:,13]==batsman]
     return len(deliveries)
-get_total_deliveries_played(b'ST Jayasuriya')
+
 
 
 
