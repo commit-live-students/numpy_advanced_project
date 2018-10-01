b6de1470854bb7defefd30b0cff5ccb753a7632a
iampa1 <iampa1gehaney@gmail.com>
2018-10-01 06:14:02 +0000

    Done

--- q01_get_total_deliveries_players/build.py
+++ q01_get_total_deliveries_players/build.py
@@ -1,7 +1,22 @@
+# %load q01_get_total_deliveries_players/build.py
 # Default imports
 import numpy as np
-
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+import pandas as pd
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
 
 # Your Solution
+def get_total_deliveries_played(name):
+    y=(ipl_matches_array)[:,13]
+    s=0
+    for x in y:
+        if x==name:
+            s+=1
+    return s
+    
+
+
+
+
+
+
 
