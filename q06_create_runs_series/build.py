34eaeb8e57e70f2aade7749ce0c471285c3cbc86
mario-the-legend <mario_the_legend@yahoo.co.in>
2018-10-02 06:00:51 +0000

    Done

--- q06_create_runs_series/build.py
+++ q06_create_runs_series/build.py
@@ -1,7 +1,14 @@
+# %load q06_create_runs_series/build.py
 #Default Imports
 import pandas as pd
 import numpy as np
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
 
 #Your Solution
+def create_runs_series(match_code):
+    blt = ipl_matches_array[(ipl_matches_array[:,0] == match_code)]
+    return pd.Series(blt[:,11],index=blt[:,16])
+    
+create_runs_series(b'392203')
+
 
