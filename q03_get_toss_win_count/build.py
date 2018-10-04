2fc691c8dab67a75984fbcf447c1c9a7315a3830
PoojaTatkare <poojatatkare8@gmail.com>
2018-10-04 19:48:03 +0000

    Done

--- q03_get_toss_win_count/build.py
+++ q03_get_toss_win_count/build.py
@@ -9,8 +9,9 @@ def get_toss_win_count(team):
     team1 = np.unique(ipl_matches_array[:,4])
     team2= np.unique(ipl_matches_array[:,5])
     win_toss= ipl_matches_array[:,6]
-    a = len(team1[team1== team])
-    b=  len(team2[team2== team])
+    a =len(team1[team1==team ])
+    b= len(team2[team2==team])
+    
     return (a+b)
 get_toss_win_count(b'Mumbai Indians')
 
--- q04_get_all_sixes_filter/build.py
+++ q04_get_all_sixes_filter/build.py
@@ -1,7 +1,15 @@
+# %load q04_get_all_sixes_filter/build.py
 #Default Imports
 import numpy as np
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
 
 
 #Your Solution
+def get_all_sixes_filter():
+    a = ipl_matches_array[:,16].astype(np.int16) 
+    b = np.array(filter(lambda x:x==6, a))
+    c = a==6
+    return c
+get_all_sixes_filter()
+
 
