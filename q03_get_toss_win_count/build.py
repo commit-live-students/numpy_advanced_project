1d170db6793a242d41755618908c8b5f33706f7d
sskewal <sskewal@yahoo.in>
2018-10-22 05:21:26 +0000

    Done

--- q03_get_toss_win_count/build.py
+++ q03_get_toss_win_count/build.py
@@ -1,7 +1,20 @@
+# %load q03_get_toss_win_count/build.py
 #Default Imports
 import numpy as np
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+import pandas as pd
 
+toss_team = b'Mumbai Indians'
 
-#Your Solution
+def get_toss_win_count(team_name):
+
+    ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
+    count=0
+    match_code = np.unique(ipl_matches_array[:,0])
+    for i in match_code:
+        if ipl_matches_array[np.argmax(ipl_matches_array[:,0]==i),5] == team_name:
+            count= count + 1
+    return count
+
+get_toss_win_count(toss_team)
+
 
