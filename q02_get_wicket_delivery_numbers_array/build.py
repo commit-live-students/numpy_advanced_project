e7bc5548c0b39f9b0f48170d7b191db491135525
seekingpeace <siddhantjain.j@gmail.com>
2018-11-03 23:17:44 +0000

    Done

--- q02_get_wicket_delivery_numbers_array/build.py
+++ q02_get_wicket_delivery_numbers_array/build.py
@@ -1,7 +1,14 @@
+# %load q02_get_wicket_delivery_numbers_array/build.py
 #Default Imports
 import numpy as np
 
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 
 #Your Solution
+def get_wicket_delivery_numbers_array(player) :
+    return ipl_matches_array[ipl_matches_array[:,-3].astype('|S50') == player,-12]
+ipl_matches_array[ipl_matches_array[:,-3].astype('|S50') == b'ST Jayasuriya',-12]
+(ipl_matches_array[:,-3].astype('|S50') == b'ST Jayasuriya').sum()
+get_wicket_delivery_numbers_array(b'ST Jayasuriya')
+
 
