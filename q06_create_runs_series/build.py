4069bc36388393a3ea2fa22614b8ec6413b6f301
seekingpeace <siddhantjain.j@gmail.com>
2018-11-04 00:37:54 +0000

    Done

--- q06_create_runs_series/build.py
+++ q06_create_runs_series/build.py
@@ -1,7 +1,16 @@
+# %load q06_create_runs_series/build.py
 #Default Imports
+from greyatomlib.numpy_advanced.q01_get_total_deliveries_players.build import ipl_matches_array
 import pandas as pd
-import numpy as np
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
+
 
 #Your Solution
+def create_runs_series(match_code) :
+    req_match = ipl_matches_array[ipl_matches_array[:,0].astype('|S50') == match_code]
+    return pd.Series(req_match[:,-7], index = req_match[:,-12])
+#392203
+#ipl_matches_array[ipl_matches_array[:,0].astype('|S50') == match_code]
+#len(ipl_matches_array)
+create_runs_series(b'392203')
+
 
