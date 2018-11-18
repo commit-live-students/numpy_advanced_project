23243df715f2dc647fc58cb95a008f94779c151b
ashpandey2509 <ashpandey@yahoo.com>
2018-11-18 10:30:57 +0000

    Done

--- q05_create_delivery_series/build.py
+++ q05_create_delivery_series/build.py
@@ -1,7 +1,13 @@
-#Default Imports
-import pandas as pd
-import numpy as np
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
-#Your Solution
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
+
+# Your Solution
+def create_delivery_series():
+    df = pd.Series(ipl_matches_array[:, 11])
+    return df
+create_delivery_series()
+
 
