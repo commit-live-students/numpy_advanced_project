55c42c6f832d1f820c29808a6010400ac721403e
ashpandey2509 <ashpandey@yahoo.com>
2018-11-18 10:23:24 +0000

    Done

--- q04_get_all_sixes_filter/build.py
+++ q04_get_all_sixes_filter/build.py
@@ -1,7 +1,13 @@
-#Default Imports
+
 import numpy as np
-ipl_matches_array =np.genfromtxt("data/ipl_matches_small.csv", dtype="|S50", skip_header=1, delimiter=",")
 
+ipl_matches_array =np.genfromtxt('data/ipl_matches_small.csv', dtype='|S50', skip_header=1, delimiter=',')
 
-#Your Solution
+# Your Solution
+def get_all_sixes_filter():
+    run_scored = ipl_matches_array[:, 16] == (b'6')
+    print (run_scored)
+    return run_scored
+get_all_sixes_filter()
+
 
